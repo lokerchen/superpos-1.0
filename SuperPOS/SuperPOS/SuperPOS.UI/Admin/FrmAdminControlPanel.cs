b4e5de9546e0622110777f1397679517f2cb8ce6
Loker2015 <Loker2015@d7ed1076-26fe-4ada-867f-e07a5824a29f>
2015-07-28 10:04:32 +0000

    后台主界面

--- SuperPOS/SuperPOS/SuperPOS.UI/Admin/FrmAdminControlPanel.cs
+++ SuperPOS/SuperPOS/SuperPOS.UI/Admin/FrmAdminControlPanel.cs
@@ -6,14 +6,29 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using SuperPOS.Domain.Entities;
 
 namespace SuperPOS.UI.Admin
 {
     public partial class FrmAdminControlPanel : Form
     {
+        private UserInfo usrInfo = new UserInfo();
+
         public FrmAdminControlPanel()
         {
             InitializeComponent();
+        }
+
+        public FrmAdminControlPanel(UserInfo uInfo)
+        {
+            InitializeComponent();
+            usrInfo = uInfo;
+        }
+
+        private void FrmAdminControlPanel_Load(object sender, EventArgs e)
+        {
+            if (usrInfo.UsrRight == 1)
+                gupBoxEatInSetting.Visible = false;
         }
     }
 }
