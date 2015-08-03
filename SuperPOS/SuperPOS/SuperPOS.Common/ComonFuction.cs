7d6c0f236d0abf0deffbd32d9b01aef446ddf820
Loker2015 <Loker2015@d7ed1076-26fe-4ada-867f-e07a5824a29f>
2015-08-03 10:46:16 +0000

    增加一个公共类

---
+++ SuperPOS/SuperPOS/SuperPOS.Common/ComonFuction.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SuperPOS.Common
+{
+    public class ComonFuction
+    {
+        public enum Week
+        {
+            Monday = 1,
+            Tuesday = 2,
+            Wednesday = 3,
+            Thursday = 4,
+            Friday = 5,
+            Saturday = 6,
+            Sunday = 7,
+        }
+    }
+}
