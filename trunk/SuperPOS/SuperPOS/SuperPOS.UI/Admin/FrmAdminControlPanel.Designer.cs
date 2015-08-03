13d484faeda898eb5b2f0f7216c40230cf3d8fd6
Loker2015 <Loker2015@d7ed1076-26fe-4ada-867f-e07a5824a29f>
2015-08-03 05:15:03 +0000

    添加Exit按钮退出等登录界面按钮事件

--- trunk/SuperPOS/SuperPOS/SuperPOS.UI/Admin/FrmAdminControlPanel.Designer.cs
+++ trunk/SuperPOS/SuperPOS/SuperPOS.UI/Admin/FrmAdminControlPanel.Designer.cs
@@ -109,6 +109,7 @@
             this.btnExit.TabIndex = 4;
             this.btnExit.Text = "Exit";
             this.btnExit.UseVisualStyleBackColor = false;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
             // 
             // grpBoxReporting
             // 
