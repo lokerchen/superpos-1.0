26e367bac7ad69ac5b35ed044d47ec42d4ed4dc0
Loker2015 <Loker2015@d7ed1076-26fe-4ada-867f-e07a5824a29f>
2015-08-11 16:05:53 +0000

    新增一个中心数据处理文件类：DALCommon.cs

---
+++ SuperPOS/SuperPOS/SuperPOS.DAL/DALCommon.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SuperPOS.DAL
+{
+    public static class DALCommon
+    {
+        #region 加载所有系统数据，所有系统List
+        /// <summary>
+        ///  加载所有系统数据，所有系统List
+        /// </summary>
+        public static void GetAllInfoList()
+        {
+            OnLoadSystemCommonData onLoad = new OnLoadSystemCommonData();
+            //User List
+            onLoad.GetUserList();
+            //Shift Code
+            onLoad.GetShiftCodeList();
+            //System Configuration
+            onLoad.GetSysConfigList();
+        }
+        #endregion
+    }
+}
