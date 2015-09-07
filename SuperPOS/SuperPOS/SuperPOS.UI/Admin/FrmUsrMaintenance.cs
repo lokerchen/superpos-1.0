94047b4e8a6b070e2db6723267d476299e42d736
Loker2015 <Loker2015@d7ed1076-26fe-4ada-867f-e07a5824a29f>
2015-09-07 07:18:27 +0000

    新增User Maintenance窗口

---
+++ SuperPOS/SuperPOS/SuperPOS.UI/Admin/FrmUsrMaintenance.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace SuperPOS.UI.Admin
+{
+    public partial class FrmUsrMaintenance : Form
+    {
+        public FrmUsrMaintenance()
+        {
+            InitializeComponent();
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Hide();
+            FrmAdminControlPanel frmAdminControl = new FrmAdminControlPanel();
+            frmAdminControl.ShowDialog();
+        }
+    }
+}
