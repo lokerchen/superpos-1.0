2b2b9df6488039c617db91815fd33dfa06c5f3a4
Loker2015 <Loker2015@d7ed1076-26fe-4ada-867f-e07a5824a29f>
2015-09-20 10:17:08 +0000

    添加User Maintenance数据访问

--- trunk/SuperPOS/SuperPOS/SuperPOS.DAL/CommonData.cs
+++ trunk/SuperPOS/SuperPOS/SuperPOS.DAL/CommonData.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,22 +9,12 @@ namespace SuperPOS.DAL
 {
     public class CommonData
     {
-        public static IList<UserInfo> UsrList
-        {
-            get;
-            set;
-        }
+        public static IList<UserInfo> UsrList { get; set; }
 
-        public static IList<ShiftCodeInfo> ShiftCodeList
-        {
-            get;
-            set;
-        }
+        public static IList<ShiftCodeInfo> ShiftCodeList { get; set; }
 
-        public static IList<SysConfigInfo> SysConfigList
-        {
-            get;
-            set;
-        }
+        public static IList<SysConfigInfo> SysConfigList { get; set; }
+
+        public static IList<UsrMaintenanceInfo> UsrMaintenanceList { get; set; }
     }
 }
