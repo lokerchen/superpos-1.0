7a922cc53fab0a9d8fcd09844f3633f76bb2dc13
Loker2015 <Loker2015@d7ed1076-26fe-4ada-867f-e07a5824a29f>
2015-09-20 16:32:15 +0000

    增加User Maintenance中的Edit User用户界面

---
+++ SuperPOS/SuperPOS/SuperPOS.UI/Admin/FrmUser.Designer.cs
@@ -0,0 +1,222 @@
+﻿namespace SuperPOS.UI.Admin
+{
+    partial class FrmUser
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.lblUsrCode = new System.Windows.Forms.Label();
+            this.lblUsrName = new System.Windows.Forms.Label();
+            this.lblUsrPwd = new System.Windows.Forms.Label();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnDel = new System.Windows.Forms.Button();
+            this.btnEdit = new System.Windows.Forms.Button();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.txtUsrCode = new System.Windows.Forms.TextBox();
+            this.txtUsrName = new System.Windows.Forms.TextBox();
+            this.txtUsrPwd = new System.Windows.Forms.TextBox();
+            this.dgvUserList = new System.Windows.Forms.DataGridView();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvUserList)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.dgvUserList);
+            this.groupBox1.Controls.Add(this.txtUsrPwd);
+            this.groupBox1.Controls.Add(this.txtUsrName);
+            this.groupBox1.Controls.Add(this.txtUsrCode);
+            this.groupBox1.Controls.Add(this.btnExit);
+            this.groupBox1.Controls.Add(this.btnSave);
+            this.groupBox1.Controls.Add(this.btnDel);
+            this.groupBox1.Controls.Add(this.btnEdit);
+            this.groupBox1.Controls.Add(this.btnAdd);
+            this.groupBox1.Controls.Add(this.lblUsrPwd);
+            this.groupBox1.Controls.Add(this.lblUsrName);
+            this.groupBox1.Controls.Add(this.lblUsrCode);
+            this.groupBox1.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(610, 314);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "User List";
+            // 
+            // lblUsrCode
+            // 
+            this.lblUsrCode.AutoSize = true;
+            this.lblUsrCode.Location = new System.Drawing.Point(333, 72);
+            this.lblUsrCode.Name = "lblUsrCode";
+            this.lblUsrCode.Size = new System.Drawing.Size(88, 16);
+            this.lblUsrCode.TabIndex = 1;
+            this.lblUsrCode.Text = "User Code:";
+            // 
+            // lblUsrName
+            // 
+            this.lblUsrName.AutoSize = true;
+            this.lblUsrName.Location = new System.Drawing.Point(333, 114);
+            this.lblUsrName.Name = "lblUsrName";
+            this.lblUsrName.Size = new System.Drawing.Size(88, 16);
+            this.lblUsrName.TabIndex = 2;
+            this.lblUsrName.Text = "User Name:";
+            // 
+            // lblUsrPwd
+            // 
+            this.lblUsrPwd.AutoSize = true;
+            this.lblUsrPwd.Location = new System.Drawing.Point(301, 159);
+            this.lblUsrPwd.Name = "lblUsrPwd";
+            this.lblUsrPwd.Size = new System.Drawing.Size(120, 16);
+            this.lblUsrPwd.TabIndex = 3;
+            this.lblUsrPwd.Text = "User Password:";
+            // 
+            // btnExit
+            // 
+            this.btnExit.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.btnExit.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnExit.ForeColor = System.Drawing.Color.White;
+            this.btnExit.Location = new System.Drawing.Point(485, 230);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(75, 69);
+            this.btnExit.TabIndex = 20;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = false;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.btnSave.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnSave.ForeColor = System.Drawing.Color.White;
+            this.btnSave.Location = new System.Drawing.Point(323, 269);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 30);
+            this.btnSave.TabIndex = 19;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = false;
+            // 
+            // btnDel
+            // 
+            this.btnDel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.btnDel.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnDel.ForeColor = System.Drawing.Color.White;
+            this.btnDel.Location = new System.Drawing.Point(404, 269);
+            this.btnDel.Name = "btnDel";
+            this.btnDel.Size = new System.Drawing.Size(75, 30);
+            this.btnDel.TabIndex = 18;
+            this.btnDel.Text = "Delete";
+            this.btnDel.UseVisualStyleBackColor = false;
+            // 
+            // btnEdit
+            // 
+            this.btnEdit.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.btnEdit.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnEdit.ForeColor = System.Drawing.Color.White;
+            this.btnEdit.Location = new System.Drawing.Point(404, 230);
+            this.btnEdit.Name = "btnEdit";
+            this.btnEdit.Size = new System.Drawing.Size(75, 30);
+            this.btnEdit.TabIndex = 17;
+            this.btnEdit.Text = "Edit";
+            this.btnEdit.UseVisualStyleBackColor = false;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.btnAdd.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnAdd.ForeColor = System.Drawing.Color.White;
+            this.btnAdd.Location = new System.Drawing.Point(323, 230);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(75, 30);
+            this.btnAdd.TabIndex = 16;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = false;
+            // 
+            // txtUsrCode
+            // 
+            this.txtUsrCode.Location = new System.Drawing.Point(427, 62);
+            this.txtUsrCode.Name = "txtUsrCode";
+            this.txtUsrCode.Size = new System.Drawing.Size(143, 26);
+            this.txtUsrCode.TabIndex = 21;
+            // 
+            // txtUsrName
+            // 
+            this.txtUsrName.Location = new System.Drawing.Point(427, 106);
+            this.txtUsrName.Name = "txtUsrName";
+            this.txtUsrName.Size = new System.Drawing.Size(143, 26);
+            this.txtUsrName.TabIndex = 22;
+            // 
+            // txtUsrPwd
+            // 
+            this.txtUsrPwd.Location = new System.Drawing.Point(427, 149);
+            this.txtUsrPwd.Name = "txtUsrPwd";
+            this.txtUsrPwd.Size = new System.Drawing.Size(143, 26);
+            this.txtUsrPwd.TabIndex = 23;
+            // 
+            // dgvUserList
+            // 
+            this.dgvUserList.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvUserList.Location = new System.Drawing.Point(15, 25);
+            this.dgvUserList.Name = "dgvUserList";
+            this.dgvUserList.RowTemplate.Height = 23;
+            this.dgvUserList.Size = new System.Drawing.Size(257, 274);
+            this.dgvUserList.TabIndex = 24;
+            // 
+            // FrmUser
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(636, 345);
+            this.Controls.Add(this.groupBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "FrmUser";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "FrmUser";
+            this.Load += new System.EventHandler(this.FrmUser_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvUserList)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label lblUsrPwd;
+        private System.Windows.Forms.Label lblUsrName;
+        private System.Windows.Forms.Label lblUsrCode;
+        private System.Windows.Forms.TextBox txtUsrPwd;
+        private System.Windows.Forms.TextBox txtUsrName;
+        private System.Windows.Forms.TextBox txtUsrCode;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnDel;
+        private System.Windows.Forms.Button btnEdit;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.DataGridView dgvUserList;
+    }
+}
---
+++ SuperPOS/SuperPOS/SuperPOS.UI/Admin/FrmUser.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using SuperPOS.DAL;
+
+namespace SuperPOS.UI.Admin
+{
+    public partial class FrmUser : Form
+    {
+        public FrmUser()
+        {
+            InitializeComponent();
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+        }
+
+        private void FrmUser_Load(object sender, EventArgs e)
+        {
+            new OnLoadSystemCommonData().GetShiftCodeList();
+            dgvUserList.DataSource = CommonData.UsrList;
+
+            dgvUserList.Columns[1].Width = 60;
+            dgvUserList.Columns[0].Visible = false;
+            dgvUserList.Columns[4].Visible = false;
+        }
+    }
+}
