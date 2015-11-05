3acab6785cc6565fb59d2b6794659081a926d9c1
superpos <superpos@d7ed1076-26fe-4ada-867f-e07a5824a29f>
2015-11-05 09:07:05 +0000

    增加Take Away版面及相应界面

---
+++ SuperPOS/SuperPOS/SuperPOS.Domain/Entities/TADistChargeRemarkInfo.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace SuperPOS.Domain.Entities
+{
+    public class TADistChargeRemarkInfo
+    {
+        [XmlElement(ElementName = "SystemKey")]
+        public virtual Guid SystemKey { get; set; }
+
+        //Code
+        [XmlElement(ElementName = "PerMileThereafter")]
+        public virtual string PerMileThereafter { get; set; }
+
+        //Computer Name
+        [XmlElement(ElementName = "OrderThreshold")]
+        public virtual string OrderThreshold { get; set; }
+
+        //Add/Edit DateTime
+        [XmlElement(ElementName = "SurchargeAmount")]
+        public virtual string SurchargeAmount { get; set; }
+
+        //Add/Edit DateTime
+        [XmlElement(ElementName = "DeliveryChargeIncludesInOrder")]
+        public virtual string DeliveryChargeIncludesInOrder { get; set; }
+
+        //Add/Edit DateTime
+        [XmlElement(ElementName = "IgnoreDeliverySurchargeWhenDistExceeds")]
+        public virtual string IgnoreDeliverySurchargeWhenDistExceeds { get; set; }
+
+        //Add/Edit DateTime
+        [XmlElement(ElementName = "IgnoreDeliveryDistChargeIfSurchargeAppl")]
+        public virtual string IgnoreDeliveryDistChargeIfSurchargeAppl { get; set; }
+
+        //Remark
+        [XmlElement(ElementName = "Remark")]
+        public virtual string Remark { get; set; }
+    }
+}
---
+++ SuperPOS/SuperPOS/SuperPOS.Domain/Entities/TAPostCodeInfo.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace SuperPOS.Domain.Entities
+{
+    public class TAPostCodeInfo
+    {
+        [XmlElement(ElementName = "SystemKey")]
+        public virtual Guid SystemKey { get; set; }
+
+        //Code
+        [XmlElement(ElementName = "PostCode")]
+        public virtual string PostCode { get; set; }
+
+        //Computer Name
+        [XmlElement(ElementName = "Address")]
+        public virtual string Address { get; set; }
+
+        //Add/Edit DateTime
+        [XmlElement(ElementName = "Map")]
+        public virtual string Map { get; set; }
+
+        [XmlElement(ElementName = "Distance")]
+        public virtual string Distance { get; set; }
+
+        //Remark
+        [XmlElement(ElementName = "Remark")]
+        public virtual string Remark { get; set; }
+    }
+}
