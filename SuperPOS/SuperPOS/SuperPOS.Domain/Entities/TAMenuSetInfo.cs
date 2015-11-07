70a288bf80e2270ca5a05b801dea390c37a6966f
superpos <superpos@d7ed1076-26fe-4ada-867f-e07a5824a29f>
2015-11-07 10:46:02 +0000

    新增Menu set

---
+++ SuperPOS/SuperPOS/SuperPOS.Domain/Entities/TAMenuSetInfo.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace SuperPOS.Domain.Entities
+{
+    public class TAMenuSetInfo
+    {
+        [XmlElement(ElementName = "SystemKey")]
+        public virtual Guid SystemKey { get; set; }
+
+        //Code
+        [XmlElement(ElementName = "EnglishName")]
+        public virtual string EnglishName { get; set; }
+
+        //Computer Name
+        [XmlElement(ElementName = "OtherName")]
+        public virtual string OtherName { get; set; }
+
+        //Remark
+        [XmlElement(ElementName = "Remark")]
+        public virtual string Remark { get; set; }
+    }
+}
