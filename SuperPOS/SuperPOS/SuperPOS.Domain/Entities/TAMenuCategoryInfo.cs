73305bb9622fe04adc91b7d82b4ebbfc75442cf9
Loker2015 <Loker2015@d7ed1076-26fe-4ada-867f-e07a5824a29f>
2015-11-15 17:02:57 +0000

    增加Take away Menu Category

--- SuperPOS/SuperPOS/SuperPOS.Domain/Entities/TAMenuCategoryInfo.cs
+++ SuperPOS/SuperPOS/SuperPOS.Domain/Entities/TAMenuCategoryInfo.cs
@@ -2,10 +2,41 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Xml.Serialization;
 
 namespace SuperPOS.Domain.Entities
 {
     public class TAMenuCategoryInfo
     {
+        [XmlElement(ElementName = "SystemKey")]
+        public virtual Guid SystemKey { get; set; }
+
+        //Code
+        [XmlElement(ElementName = "EnglishName")]
+        public virtual string EnglishName { get; set; }
+
+        //Computer Name
+        [XmlElement(ElementName = "OtherName")]
+        public virtual string OtherName { get; set; }
+
+        //Add/Edit DateTime
+        [XmlElement(ElementName = "DisplayPosition")]
+        public virtual string DisplayPosition { get; set; }
+
+        //Remark
+        [XmlElement(ElementName = "DeptSysKey")]
+        public virtual string DeptSysKey { get; set; }
+
+        //Remark
+        [XmlElement(ElementName = "DeptCode")]
+        public virtual string DeptCode { get; set; }
+
+        //Remark
+        [XmlElement(ElementName = "IsHotKey")]
+        public virtual string IsHotKey { get; set; }
+
+        //Remark
+        [XmlElement(ElementName = "DishCode")]
+        public virtual string DishCode { get; set; }
     }
 }
