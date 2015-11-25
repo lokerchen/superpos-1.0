45c62212179411ae0725d9357ddd59043706da23
Loker2015 <Loker2015@d7ed1076-26fe-4ada-867f-e07a5824a29f>
2015-11-25 16:53:57 +0000

    增加登录后的菜单选择窗口FrmSelectMenu和订单显示窗口FrmShowOrder

---
+++ SuperPOS/SuperPOS/SuperPOS.UI/FrmSelectMenu.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using SuperPOS.Domain.Entities;
+using SuperPOS.UI.Admin;
+using SuperPOS.UI.TakeAway;
+
+namespace SuperPOS.UI
+{
+    public partial class FrmSelectMenu : Form
+    {
+        private UserInfo userInfo = new UserInfo();
+
+        public FrmSelectMenu()
+        {
+            InitializeComponent();
+        }
+
+        public FrmSelectMenu(UserInfo user)
+        {
+            InitializeComponent();
+            userInfo = user;
+        }
+
+        private void btnTakeaway_Click(object sender, EventArgs e)
+        {
+            Hide();
+            FrmTAMain frmTaMain = new FrmTAMain(userInfo);
+            frmTaMain.ShowDialog();
+        }
+
+        private void btnEatIn_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void btnQuickFood_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        #region 登出
+        private void btnLogout_Click(object sender, EventArgs e)
+        {
+            Hide();
+            FrmLogon frmLogon = new FrmLogon();
+            frmLogon.ShowDialog();
+        }
+        #endregion
+
+        #region Control Panel按钮
+        private void btnCtlPanel_Click(object sender, EventArgs e)
+        {
+            Hide();
+            FrmAdminControlPanel frmAdminControl = new FrmAdminControlPanel();
+            frmAdminControl.ShowDialog();
+        }
+        #endregion
+        
+
+        private void FrmSelectMenu_Load(object sender, EventArgs e)
+        {
+            txtDate.Text = DateTime.Now.ToShortDateString();
+            txtTime.Text = DateTime.Now.ToLongTimeString();
+        }
+
+        #region 秒表变化
+        private void timerShow_Tick(object sender, EventArgs e)
+        {
+            txtTime.Text = DateTime.Now.ToLongTimeString();
+        }
+        #endregion
+
+        #region 按钮变化
+
+        #region Takeaway按钮特效，鼠标经过时变更按钮颜色
+        private void btnTakeaway_MouseMove(object sender, MouseEventArgs e)
+        {
+            btnTakeaway.BackColor = Color.RoyalBlue;
+        }
+        #endregion
+
+        #region Takeaway按钮特效，鼠标离开时变回原颜色
+        private void btnTakeaway_MouseLeave(object sender, EventArgs e)
+        {
+            btnTakeaway.BackColor = Color.Silver;
+        }
+        #endregion
+
+        #region Eat In按钮特效，鼠标经过时变更按钮颜色
+        private void btnEatIn_MouseMove(object sender, MouseEventArgs e)
+        {
+            btnEatIn.BackColor = Color.RoyalBlue;
+        }
+        #endregion
+
+        #region Eat In按钮特效，鼠标离开时变回原颜色
+        private void btnEatIn_MouseLeave(object sender, EventArgs e)
+        {
+            btnEatIn.BackColor = Color.Silver;
+        }
+        #endregion
+
+        #region Quick Food按钮特效，鼠标经过时变更按钮颜色
+        private void btnQuickFood_MouseMove(object sender, MouseEventArgs e)
+        {
+            btnQuickFood.BackColor = Color.RoyalBlue;
+        }
+        #endregion
+
+        #region Quick Food按钮特效，鼠标离开时变回原颜色
+        private void btnQuickFood_MouseLeave(object sender, EventArgs e)
+        {
+            btnQuickFood.BackColor = Color.Silver;
+        }
+        #endregion
+
+        #endregion
+
+        private void btnShowOrder_Click(object sender, EventArgs e)
+        {
+            Hide();
+            FrmShowOrder frmShowOrder = new FrmShowOrder(userInfo);
+            frmShowOrder.ShowDialog();
+        }
+    }
+}
---
+++ SuperPOS/SuperPOS/SuperPOS.UI/FrmShowOrder.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using SuperPOS.Domain.Entities;
+
+namespace SuperPOS.UI
+{
+    public partial class FrmShowOrder : Form
+    {
+        private UserInfo userInfo = new UserInfo();
+
+        public FrmShowOrder()
+        {
+            InitializeComponent();
+        }
+
+        public FrmShowOrder(UserInfo user)
+        {
+            InitializeComponent();
+            userInfo = user;
+        }
+    }
+}
