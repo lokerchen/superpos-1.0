8a25fb5711ab7d42b55a41447d2217670e4573c0
Loker2015 <Loker2015@d7ed1076-26fe-4ada-867f-e07a5824a29f>
2015-12-16 12:47:58 +0000

    无修改

--- SuperPOS/SuperPOS/SuperPOS.Common/CommonFuction.cs
+++ SuperPOS/SuperPOS/SuperPOS.Common/CommonFuction.cs
@@ -2,20 +2,27 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using SuperPOS.Domain.Entities;
 
 namespace SuperPOS.Common
 {
-    public class CommonFuction
+    public static class CommonFuction
     {
-        public enum Week
-        {
-            Monday = 1,
-            Tuesday = 2,
-            Wednesday = 3,
-            Thursday = 4,
-            Friday = 5,
-            Saturday = 6,
-            Sunday = 7,
-        }
+        //public enum Week
+        //{
+        //    Monday = 1,
+        //    Tuesday = 2,
+        //    Wednesday = 3,
+        //    Thursday = 4,
+        //    Friday = 5,
+        //    Saturday = 6,
+        //    Sunday = 7,
+        //}
+
+        ////获得用户权限
+        //public static bool GetAuthority(UserInfo user)
+        //{
+        //    CommonData.
+        //}
     }
 }
