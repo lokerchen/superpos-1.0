4bdfc993681720b0d9f476691407ec6df422e8b2
Loker2015 <Loker2015@d7ed1076-26fe-4ada-867f-e07a5824a29f>
2015-12-22 16:01:48 +0000

    增加带参数构造函数

--- SuperPOS/SuperPOS/SuperPOS.UI/TakeAway/FrmTACustInfo.cs
+++ SuperPOS/SuperPOS/SuperPOS.UI/TakeAway/FrmTACustInfo.cs
@@ -11,9 +11,16 @@ namespace SuperPOS.UI.TakeAway
 {
     public partial class FrmTACustInfo : Form
     {
+        private string sCallID = "";
         public FrmTACustInfo()
         {
             InitializeComponent();
+        }
+
+        public FrmTACustInfo(string strCallID)
+        {
+            InitializeComponent();
+            sCallID = strCallID;
         }
 
         private void btnExit_Click(object sender, EventArgs e)
