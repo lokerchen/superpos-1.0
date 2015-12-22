588e01434442c6e36b8b650b9d449e6fe6606ad0
Loker2015 <Loker2015@d7ed1076-26fe-4ada-867f-e07a5824a29f>
2015-12-22 16:03:47 +0000

    增加公用方法类

---
+++ SuperPOS/SuperPOS/SuperPOS.DAL/CommonFunction.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NHibernate.Criterion;
+using SuperPOS.Common;
+using SuperPOS.Domain.Entities;
+
+namespace SuperPOS.DAL
+{
+    public static class CommonFunction
+    {
+        public static bool GetAuthorityGSSysConf(string strPwd)
+        {
+            new OnLoadSystemCommonData().GetUsrMaintenance();
+            new OnLoadSystemCommonData().GetUserList();
+
+            var uList = CommonData.UsrMaintenanceList.Join(CommonData.UsrList, umList => umList.UsrCode,
+                usrList => usrList.UsrCode, (umList, usrList) => new {uPwd = usrList.UsrPwd, uGSSSysConfig = umList.GSSSystemConfig });
+
+            if (!uList.Any())
+                return false;
+            else
+            {
+                return uList.Any(s => s.uPwd.Equals(strPwd) && s.uGSSSysConfig.Equals("Y"));
+            }
+            //var uList = CommonData.UsrMaintenanceList.Where(s => s.UsrCode.Equals(strPwd));
+
+            //if (uList == null || !uList.Any())
+            //    return false;
+            //else
+            //    return uList.FirstOrDefault().GSSSystemConfig.Equals("Y");
+            //return strPwd.Equals(CommonBase.SYS_CONTROL_PWD) ||
+            //       uList.Any() && uList.FirstOrDefault().GSSSystemConfig.Equals("Y");
+        }
+    }
+}
