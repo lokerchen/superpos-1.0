534ec6050beedc59c6b61e59f2c3c6dc845aa8a8
Loker2015 <Loker2015@d7ed1076-26fe-4ada-867f-e07a5824a29f>
2016-03-10 03:40:37 +0000

    界面优化和增加参数设置

--- SuperPOS/SuperPOS/SuperPOS.UI/Admin/FrmSysCtl.Designer.cs
+++ SuperPOS/SuperPOS/SuperPOS.UI/Admin/FrmSysCtl.Designer.cs
@@ -75,11 +75,12 @@
             this.groupBox1.Controls.Add(this.chkQuickFood);
             this.groupBox1.Controls.Add(this.chkEatIn);
             this.groupBox1.Controls.Add(this.chkTakeaway);
-            this.groupBox1.Location = new System.Drawing.Point(14, 40);
+            this.groupBox1.Font = new System.Drawing.Font("微软雅黑", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.groupBox1.Location = new System.Drawing.Point(14, 20);
             this.groupBox1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.groupBox1.Name = "groupBox1";
             this.groupBox1.Padding = new System.Windows.Forms.Padding(3, 4, 3, 4);
-            this.groupBox1.Size = new System.Drawing.Size(369, 74);
+            this.groupBox1.Size = new System.Drawing.Size(369, 64);
             this.groupBox1.TabIndex = 0;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Function Access Control";
@@ -87,10 +88,10 @@
             // chkQuickFood
             // 
             this.chkQuickFood.AutoSize = true;
-            this.chkQuickFood.Location = new System.Drawing.Point(246, 43);
+            this.chkQuickFood.Location = new System.Drawing.Point(246, 27);
             this.chkQuickFood.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.chkQuickFood.Name = "chkQuickFood";
-            this.chkQuickFood.Size = new System.Drawing.Size(117, 25);
+            this.chkQuickFood.Size = new System.Drawing.Size(105, 24);
             this.chkQuickFood.TabIndex = 2;
             this.chkQuickFood.Text = "Quick Food";
             this.chkQuickFood.UseVisualStyleBackColor = true;
@@ -98,10 +99,10 @@
             // chkEatIn
             // 
             this.chkEatIn.AutoSize = true;
-            this.chkEatIn.Location = new System.Drawing.Point(140, 43);
+            this.chkEatIn.Location = new System.Drawing.Point(140, 27);
             this.chkEatIn.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.chkEatIn.Name = "chkEatIn";
-            this.chkEatIn.Size = new System.Drawing.Size(73, 25);
+            this.chkEatIn.Size = new System.Drawing.Size(66, 24);
             this.chkEatIn.TabIndex = 1;
             this.chkEatIn.Text = "Eat In";
             this.chkEatIn.UseVisualStyleBackColor = true;
@@ -109,10 +110,10 @@
             // chkTakeaway
             // 
             this.chkTakeaway.AutoSize = true;
-            this.chkTakeaway.Location = new System.Drawing.Point(13, 43);
+            this.chkTakeaway.Location = new System.Drawing.Point(17, 26);
             this.chkTakeaway.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.chkTakeaway.Name = "chkTakeaway";
-            this.chkTakeaway.Size = new System.Drawing.Size(104, 25);
+            this.chkTakeaway.Size = new System.Drawing.Size(94, 24);
             this.chkTakeaway.TabIndex = 0;
             this.chkTakeaway.Text = "Takeaway";
             this.chkTakeaway.UseVisualStyleBackColor = true;
@@ -124,11 +125,12 @@
             this.groupBox2.Controls.Add(this.label2);
             this.groupBox2.Controls.Add(this.txtShopName);
             this.groupBox2.Controls.Add(this.label1);
-            this.groupBox2.Location = new System.Drawing.Point(14, 122);
+            this.groupBox2.Font = new System.Drawing.Font("微软雅黑", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.groupBox2.Location = new System.Drawing.Point(14, 92);
             this.groupBox2.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.groupBox2.Name = "groupBox2";
             this.groupBox2.Padding = new System.Windows.Forms.Padding(3, 4, 3, 4);
-            this.groupBox2.Size = new System.Drawing.Size(369, 193);
+            this.groupBox2.Size = new System.Drawing.Size(369, 146);
             this.groupBox2.TabIndex = 1;
             this.groupBox2.TabStop = false;
             this.groupBox2.Text = "Shop Details";
@@ -136,28 +138,28 @@
             // chkShopDetailReadOnly
             // 
             this.chkShopDetailReadOnly.AutoSize = true;
-            this.chkShopDetailReadOnly.Location = new System.Drawing.Point(17, 146);
+            this.chkShopDetailReadOnly.Location = new System.Drawing.Point(17, 109);
             this.chkShopDetailReadOnly.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.chkShopDetailReadOnly.Name = "chkShopDetailReadOnly";
-            this.chkShopDetailReadOnly.Size = new System.Drawing.Size(207, 25);
+            this.chkShopDetailReadOnly.Size = new System.Drawing.Size(186, 24);
             this.chkShopDetailReadOnly.TabIndex = 4;
             this.chkShopDetailReadOnly.Text = "Shop Details Read Only";
             this.chkShopDetailReadOnly.UseVisualStyleBackColor = true;
             // 
             // txtAddress
             // 
-            this.txtAddress.Location = new System.Drawing.Point(119, 89);
+            this.txtAddress.Location = new System.Drawing.Point(119, 72);
             this.txtAddress.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.txtAddress.Name = "txtAddress";
-            this.txtAddress.Size = new System.Drawing.Size(227, 29);
+            this.txtAddress.Size = new System.Drawing.Size(227, 26);
             this.txtAddress.TabIndex = 3;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(13, 97);
+            this.label2.Location = new System.Drawing.Point(39, 78);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(70, 21);
+            this.label2.Size = new System.Drawing.Size(62, 20);
             this.label2.TabIndex = 2;
             this.label2.Text = "Address";
             // 
@@ -166,15 +168,15 @@
             this.txtShopName.Location = new System.Drawing.Point(119, 33);
             this.txtShopName.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.txtShopName.Name = "txtShopName";
-            this.txtShopName.Size = new System.Drawing.Size(227, 29);
+            this.txtShopName.Size = new System.Drawing.Size(227, 26);
             this.txtShopName.TabIndex = 1;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(13, 43);
+            this.label1.Location = new System.Drawing.Point(13, 39);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(100, 21);
+            this.label1.Size = new System.Drawing.Size(88, 20);
             this.label1.TabIndex = 0;
             this.label1.Text = "Shop Name";
             // 
@@ -185,29 +187,30 @@
             this.groupBox3.Controls.Add(this.txtKey2);
             this.groupBox3.Controls.Add(this.txtKey1);
             this.groupBox3.Controls.Add(this.chkNotAccountFor);
-            this.groupBox3.Location = new System.Drawing.Point(14, 323);
+            this.groupBox3.Font = new System.Drawing.Font("微软雅黑", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.groupBox3.Location = new System.Drawing.Point(14, 244);
             this.groupBox3.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.groupBox3.Name = "groupBox3";
             this.groupBox3.Padding = new System.Windows.Forms.Padding(3, 4, 3, 4);
-            this.groupBox3.Size = new System.Drawing.Size(369, 194);
+            this.groupBox3.Size = new System.Drawing.Size(369, 164);
             this.groupBox3.TabIndex = 2;
             this.groupBox3.TabStop = false;
             this.groupBox3.Text = "Smart Control";
             // 
             // txtAccessCode
             // 
-            this.txtAccessCode.Location = new System.Drawing.Point(140, 127);
+            this.txtAccessCode.Location = new System.Drawing.Point(123, 127);
             this.txtAccessCode.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.txtAccessCode.Name = "txtAccessCode";
-            this.txtAccessCode.Size = new System.Drawing.Size(162, 29);
+            this.txtAccessCode.Size = new System.Drawing.Size(162, 26);
             this.txtAccessCode.TabIndex = 4;
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(29, 135);
+            this.label3.Location = new System.Drawing.Point(25, 133);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(105, 21);
+            this.label3.Size = new System.Drawing.Size(92, 20);
             this.label3.TabIndex = 3;
             this.label3.Text = "Access Code";
             // 
@@ -216,7 +219,7 @@
             this.txtKey2.Location = new System.Drawing.Point(145, 78);
             this.txtKey2.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.txtKey2.Name = "txtKey2";
-            this.txtKey2.Size = new System.Drawing.Size(96, 29);
+            this.txtKey2.Size = new System.Drawing.Size(96, 26);
             this.txtKey2.TabIndex = 2;
             this.txtKey2.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txtKey2_KeyDown);
             // 
@@ -225,7 +228,7 @@
             this.txtKey1.Location = new System.Drawing.Point(33, 78);
             this.txtKey1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.txtKey1.Name = "txtKey1";
-            this.txtKey1.Size = new System.Drawing.Size(84, 29);
+            this.txtKey1.Size = new System.Drawing.Size(84, 26);
             this.txtKey1.TabIndex = 1;
             this.txtKey1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txtKey1_KeyDown);
             // 
@@ -235,7 +238,7 @@
             this.chkNotAccountFor.Location = new System.Drawing.Point(33, 30);
             this.chkNotAccountFor.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.chkNotAccountFor.Name = "chkNotAccountFor";
-            this.chkNotAccountFor.Size = new System.Drawing.Size(156, 25);
+            this.chkNotAccountFor.Size = new System.Drawing.Size(138, 24);
             this.chkNotAccountFor.TabIndex = 0;
             this.chkNotAccountFor.Text = "Not Account For";
             this.chkNotAccountFor.UseVisualStyleBackColor = true;
@@ -253,7 +256,7 @@
             this.groupBox4.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.groupBox4.Name = "groupBox4";
             this.groupBox4.Padding = new System.Windows.Forms.Padding(3, 4, 3, 4);
-            this.groupBox4.Size = new System.Drawing.Size(878, 529);
+            this.groupBox4.Size = new System.Drawing.Size(878, 417);
             this.groupBox4.TabIndex = 3;
             this.groupBox4.TabStop = false;
             this.groupBox4.Text = "System Control";
@@ -264,7 +267,7 @@
             this.btnExit.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.btnExit.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
             this.btnExit.ForeColor = System.Drawing.Color.White;
-            this.btnExit.Location = new System.Drawing.Point(636, 444);
+            this.btnExit.Location = new System.Drawing.Point(656, 347);
             this.btnExit.Name = "btnExit";
             this.btnExit.Size = new System.Drawing.Size(110, 50);
             this.btnExit.TabIndex = 4;
@@ -278,7 +281,7 @@
             this.btnSave.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.btnSave.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
             this.btnSave.ForeColor = System.Drawing.Color.White;
-            this.btnSave.Location = new System.Drawing.Point(471, 444);
+            this.btnSave.Location = new System.Drawing.Point(478, 347);
             this.btnSave.Name = "btnSave";
             this.btnSave.Size = new System.Drawing.Size(110, 50);
             this.btnSave.TabIndex = 3;
@@ -302,9 +305,10 @@
             this.groupBox5.Controls.Add(this.label6);
             this.groupBox5.Controls.Add(this.label5);
             this.groupBox5.Controls.Add(this.label4);
-            this.groupBox5.Location = new System.Drawing.Point(392, 40);
+            this.groupBox5.Font = new System.Drawing.Font("微软雅黑", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.groupBox5.Location = new System.Drawing.Point(392, 20);
             this.groupBox5.Name = "groupBox5";
-            this.groupBox5.Size = new System.Drawing.Size(458, 355);
+            this.groupBox5.Size = new System.Drawing.Size(458, 289);
             this.groupBox5.TabIndex = 5;
             this.groupBox5.TabStop = false;
             this.groupBox5.Text = "System Setting";
@@ -314,25 +318,25 @@
             this.label4.AutoSize = true;
             this.label4.Location = new System.Drawing.Point(16, 26);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(376, 21);
+            this.label4.Size = new System.Drawing.Size(331, 20);
             this.label4.TabIndex = 0;
             this.label4.Text = "Welcoming / Functional choices screen set up：";
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(16, 91);
+            this.label5.Location = new System.Drawing.Point(16, 81);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(276, 21);
+            this.label5.Size = new System.Drawing.Size(243, 20);
             this.label5.TabIndex = 1;
             this.label5.Text = "Log in screen logo picture set up：";
             // 
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(16, 154);
+            this.label6.Location = new System.Drawing.Point(16, 135);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(248, 21);
+            this.label6.Size = new System.Drawing.Size(218, 20);
             this.label6.TabIndex = 2;
             this.label6.Text = "User login screen logo set up：";
             // 
@@ -341,23 +345,23 @@
             this.txtImg1.Enabled = false;
             this.txtImg1.Location = new System.Drawing.Point(20, 50);
             this.txtImg1.Name = "txtImg1";
-            this.txtImg1.Size = new System.Drawing.Size(354, 29);
+            this.txtImg1.Size = new System.Drawing.Size(354, 26);
             this.txtImg1.TabIndex = 3;
             // 
             // txtImg2
             // 
             this.txtImg2.Enabled = false;
-            this.txtImg2.Location = new System.Drawing.Point(20, 115);
+            this.txtImg2.Location = new System.Drawing.Point(20, 105);
             this.txtImg2.Name = "txtImg2";
-            this.txtImg2.Size = new System.Drawing.Size(354, 29);
+            this.txtImg2.Size = new System.Drawing.Size(354, 26);
             this.txtImg2.TabIndex = 4;
             // 
             // txtImg3
             // 
             this.txtImg3.Enabled = false;
-            this.txtImg3.Location = new System.Drawing.Point(20, 176);
+            this.txtImg3.Location = new System.Drawing.Point(20, 157);
             this.txtImg3.Name = "txtImg3";
-            this.txtImg3.Size = new System.Drawing.Size(354, 29);
+            this.txtImg3.Size = new System.Drawing.Size(354, 26);
             this.txtImg3.TabIndex = 5;
             // 
             // openFileDialog1
@@ -376,7 +380,7 @@
             // 
             // btnImg2
             // 
-            this.btnImg2.Location = new System.Drawing.Point(380, 115);
+            this.btnImg2.Location = new System.Drawing.Point(380, 105);
             this.btnImg2.Name = "btnImg2";
             this.btnImg2.Size = new System.Drawing.Size(56, 29);
             this.btnImg2.TabIndex = 7;
@@ -386,7 +390,7 @@
             // 
             // btnImg3
             // 
-            this.btnImg3.Location = new System.Drawing.Point(380, 175);
+            this.btnImg3.Location = new System.Drawing.Point(380, 156);
             this.btnImg3.Name = "btnImg3";
             this.btnImg3.Size = new System.Drawing.Size(56, 30);
             this.btnImg3.TabIndex = 8;
@@ -396,7 +400,7 @@
             // 
             // btnImg4
             // 
-            this.btnImg4.Location = new System.Drawing.Point(380, 245);
+            this.btnImg4.Location = new System.Drawing.Point(380, 207);
             this.btnImg4.Name = "btnImg4";
             this.btnImg4.Size = new System.Drawing.Size(56, 30);
             this.btnImg4.TabIndex = 11;
@@ -407,33 +411,33 @@
             // txtImg4
             // 
             this.txtImg4.Enabled = false;
-            this.txtImg4.Location = new System.Drawing.Point(20, 246);
+            this.txtImg4.Location = new System.Drawing.Point(20, 208);
             this.txtImg4.Name = "txtImg4";
-            this.txtImg4.Size = new System.Drawing.Size(354, 29);
+            this.txtImg4.Size = new System.Drawing.Size(354, 26);
             this.txtImg4.TabIndex = 10;
             // 
             // label7
             // 
             this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(16, 224);
+            this.label7.Location = new System.Drawing.Point(16, 186);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(317, 21);
+            this.label7.Size = new System.Drawing.Size(279, 20);
             this.label7.TabIndex = 9;
             this.label7.Text = "Remot Support button position set up：";
             // 
             // txtImg5
             // 
-            this.txtImg5.Location = new System.Drawing.Point(20, 314);
+            this.txtImg5.Location = new System.Drawing.Point(20, 257);
             this.txtImg5.Name = "txtImg5";
-            this.txtImg5.Size = new System.Drawing.Size(354, 29);
+            this.txtImg5.Size = new System.Drawing.Size(354, 26);
             this.txtImg5.TabIndex = 13;
             // 
             // label8
             // 
             this.label8.AutoSize = true;
-            this.label8.Location = new System.Drawing.Point(16, 292);
+            this.label8.Location = new System.Drawing.Point(16, 235);
             this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(300, 21);
+            this.label8.Size = new System.Drawing.Size(263, 20);
             this.label8.TabIndex = 12;
             this.label8.Text = "Online order website address set up：";
             // 
@@ -441,7 +445,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(901, 560);
+            this.ClientSize = new System.Drawing.Size(901, 440);
             this.Controls.Add(this.groupBox4);
             this.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
