ec4a32cb789f087c70587c999d40af14b2a9a068
Loker2015 <Loker2015@d7ed1076-26fe-4ada-867f-e07a5824a29f>
2016-03-10 03:24:41 +0000

    增加五个字段，分别为： ImgPath1：欢迎界面/功能选择界面图片设置 ImgPath2：登录界面LOGO设置 ImgPath3：登录界面客户LOGO设置 ImgPath4：remote support位置设置 ImgPath5：net shop网址设置

--- SuperPOS/SuperPOS/SuperPOS.Domain/Entities/SysControlInfo.cs
+++ SuperPOS/SuperPOS/SuperPOS.Domain/Entities/SysControlInfo.cs
@@ -8,41 +8,68 @@ namespace SuperPOS.Domain.Entities
 {
     public class SysControlInfo
     {
+        //SystemKey
         [XmlElement(ElementName = "SystemKey")]
         public virtual Guid SystemKey { get; set; }
 
-        //Code
+        //IsTakeaway
         [XmlElement(ElementName = "IsTakeaway")]
         public virtual string IsTakeaway { get; set; }
 
-        //Computer Name
+        //IsEatIn
         [XmlElement(ElementName = "IsEatIn")]
         public virtual string IsEatIn { get; set; }
 
-        //Add/Edit DateTime
+        //IsQuickFood
         [XmlElement(ElementName = "IsQuickFood")]
         public virtual string IsQuickFood { get; set; }
 
-        //Remark
+        //ShopName
         [XmlElement(ElementName = "ShopName")]
         public virtual string ShopName { get; set; }
 
+        //Address
         [XmlElement(ElementName = "Address")]
         public virtual string Address { get; set; }
 
+        //IsShopDetailReadOnly
         [XmlElement(ElementName = "IsShopDetailReadOnly")]
         public virtual string IsShopDetailReadOnly { get; set; }
 
+        //IsNotAccountFor
         [XmlElement(ElementName = "IsNotAccountFor")]
         public virtual string IsNotAccountFor { get; set; }
 
+        //Key1
         [XmlElement(ElementName = "Key1")]
         public virtual string Key1 { get; set; }
 
+        //Key2
         [XmlElement(ElementName = "Key2")]
         public virtual string Key2 { get; set; }
 
+        //AccessCode
         [XmlElement(ElementName = "AccessCode")]
         public virtual string AccessCode { get; set; }
+
+        //ImgPath1
+        [XmlElement(ElementName = "ImgPath1")]
+        public virtual string ImgPath1 { get; set; }
+
+        //ImgPath2
+        [XmlElement(ElementName = "ImgPath2")]
+        public virtual string ImgPath2 { get; set; }
+
+        //ImgPath3
+        [XmlElement(ElementName = "ImgPath3")]
+        public virtual string ImgPath3 { get; set; }
+
+        //ImgPath4
+        [XmlElement(ElementName = "ImgPath4")]
+        public virtual string ImgPath4 { get; set; }
+
+        //ImgPath5
+        [XmlElement(ElementName = "ImgPath5")]
+        public virtual string ImgPath5 { get; set; }
     }
 }
