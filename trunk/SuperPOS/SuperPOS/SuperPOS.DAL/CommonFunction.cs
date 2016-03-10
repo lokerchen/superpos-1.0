d28d4e2a5b82aabd8f4666b6140ab62845eaf461
Loker2015 <Loker2015@d7ed1076-26fe-4ada-867f-e07a5824a29f>
2016-03-10 03:21:27 +0000

    增加：获得系统设置图片和变量方法

--- trunk/SuperPOS/SuperPOS/SuperPOS.DAL/CommonFunction.cs
+++ trunk/SuperPOS/SuperPOS/SuperPOS.DAL/CommonFunction.cs
@@ -8,7 +8,7 @@ using SuperPOS.Domain.Entities;
 
 namespace SuperPOS.DAL
 {
-    public static class CommonFunction
+    public class CommonFunction
     {
         #region 判断是否有GSSSystemConfig权限
         /// <summary>
@@ -31,6 +31,46 @@ namespace SuperPOS.DAL
             //    return uList.Any(s => s.uPwd.Equals(strPwd) && s.uGSSSysConfig.Equals("Y"));
             //}
             return uList.Any() && uList.Any(s => s.uPwd.Equals(strPwd) && s.uGSSSysConfig.Equals("Y"));
+        }
+        #endregion
+
+        #region 获得系统设置图片和变量
+        /// <summary>
+        /// 获得系统设置图片和变量
+        /// </summary>
+        /// <param name="strPathNo">获得数据编号</param>
+        /// <returns></returns>
+        public static string GetSysImg(string strPathNo)
+        {
+            OnLoadSystemCommonData onLoadSystemCommonData = new OnLoadSystemCommonData();
+            onLoadSystemCommonData.GetSysControlList();
+
+            string strImg = "";
+            if (CommonData.SysControlList.Any())
+            {
+                var qList = CommonData.SysControlList.FirstOrDefault();
+
+                switch (strPathNo)
+                {
+                    case "1":
+                        strImg = qList.ImgPath1;
+                        break;
+                    case "2":
+                        strImg = qList.ImgPath2;
+                        break;
+                    case "3":
+                        strImg = qList.ImgPath3;
+                        break;
+                    case "4":
+                        strImg = qList.ImgPath4;
+                        break;
+                    case "5":
+                        strImg = qList.ImgPath5;
+                        break;
+                }
+            }
+
+            return strImg;
         }
         #endregion
     }
