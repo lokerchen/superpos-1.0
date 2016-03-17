01669617dfc5da81be4b26acda40eefa76ec72e9
Loker2015 <Loker2015@d7ed1076-26fe-4ada-867f-e07a5824a29f>
2016-03-17 03:35:57 +0000

    Exit按钮

--- SuperPOS/SuperPOS/SuperPOS.UI/TakeAway/FrmTAConfig.Designer.cs
+++ SuperPOS/SuperPOS/SuperPOS.UI/TakeAway/FrmTAConfig.Designer.cs
@@ -607,6 +607,7 @@
             this.btnExit.TabIndex = 20;
             this.btnExit.Text = "Exit";
             this.btnExit.UseVisualStyleBackColor = false;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
             // 
             // btnSave
             // 
