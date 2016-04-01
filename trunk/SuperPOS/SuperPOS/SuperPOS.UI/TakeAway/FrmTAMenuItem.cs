c0d453b534820c9acdb7748234be4e6fbec7cc79
Loker2015 <Loker2015@d7ed1076-26fe-4ada-867f-e07a5824a29f>
2016-04-01 05:52:47 +0000

    修正启动时显示画面

--- trunk/SuperPOS/SuperPOS/SuperPOS.UI/TakeAway/FrmTAMenuItem.cs
+++ trunk/SuperPOS/SuperPOS/SuperPOS.UI/TakeAway/FrmTAMenuItem.cs
@@ -6,6 +6,7 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using SuperPOS.Common;
 using SuperPOS.DAL;
 using SuperPOS.Domain.Entities;
 using SuperPOS.UI.Admin;
@@ -39,6 +40,16 @@ namespace SuperPOS.UI.TakeAway
         private Guid btnTHGuid2 = Guid.Empty;
         private Guid btnTHGuid3 = Guid.Empty;
         private Guid btnTHGuid4 = Guid.Empty;
+
+        private string menuCate1 = "";
+        private string menuCate2 = "";
+        private string menuCate3 = "";
+
+        private string menuShitCode = "";
+
+        private int iStatus = 0;
+
+        private readonly EntityControl _control = new EntityControl();
         public FrmTAMenuItem()
         {
             InitializeComponent();
@@ -55,6 +66,13 @@ namespace SuperPOS.UI.TakeAway
             OnLoadSystemCommonData onLoadSystemCommonData = new OnLoadSystemCommonData();
             onLoadSystemCommonData.GetTAMenuCategory();
             onLoadSystemCommonData.GetTAMenuItemList();
+
+            #region Menu Category下拉选择
+            //cmbBoxMenuCate1.DataSource = CommonData.TaMenuCategoryList.Select(s => s.EnglishName).ToList();
+            //cmbBoxMenuCate2.DataSource = CommonData.TaMenuCategoryList.Select(s => s.EnglishName).ToList();
+            //cmbBoxMenuCate3.DataSource = CommonData.TaMenuCategoryList.Select(s => s.EnglishName).ToList();
+            BindCmbData();
+            #endregion
 
             //cmBoxDish.DataSource = CommonData.TaMenuCategoryList.Select(s => new {});
 
@@ -215,6 +233,7 @@ namespace SuperPOS.UI.TakeAway
             #endregion
 
             onLoadSystemCommonData.GetTAMenuCategory();
+
             #region Dish Attributes category Search下拉菜单
             cmDABoxDish.DataSource = CommonData.TaMenuCategoryList.Select(s => s.EnglishName).ToList();
             #endregion
@@ -230,32 +249,36 @@ namespace SuperPOS.UI.TakeAway
             #region Taste Handling category Search下拉菜单
             cmTHBoxDish.DataSource = CommonData.TaMenuCategoryList.Select(s => s.EnglishName).ToList();
             #endregion
-
-
+            
             #region dgvMenu
             onLoadSystemCommonData.GetTAMenuItemList();
             dgvDAMenu.DataSource = CommonData.TaMenuItemList;
             #region DataGrid控制
             dgvDAMenu.Columns[0].Visible = false;
-            dgvDAMenu.Columns[2].Visible = false;
-            dgvDAMenu.Columns[4].Visible = false;
-            dgvDAMenu.Columns[5].Visible = false;
-            dgvDAMenu.Columns[6].Visible = false;
-            dgvDAMenu.Columns[7].Visible = false;
-            dgvDAMenu.Columns[8].Visible = false;
-            dgvDAMenu.Columns[9].Visible = false;
-            dgvDAMenu.Columns[10].Visible = false;
-            dgvDAMenu.Columns[11].Visible = false;
-            dgvDAMenu.Columns[12].Visible = false;
-            dgvDAMenu.Columns[13].Visible = false;
-            dgvDAMenu.Columns[14].Visible = false;
-            dgvDAMenu.Columns[15].Visible = false;
-            dgvDAMenu.Columns[16].Visible = false;
-            dgvDAMenu.Columns[17].Visible = false;
-            dgvDAMenu.Columns[18].Visible = false;
-            dgvDAMenu.Columns[19].Visible = false;
-            dgvDAMenu.Columns[20].Visible = false;
-            dgvDAMenu.Columns[21].Visible = false;
+            for (int i = 2; i < 23; i++) { dgvDAMenu.Columns[i].Visible = false; }
+            //dgvDAMenu.Columns[2].Visible = false;
+            //dgvDAMenu.Columns[4].Visible = false;
+            //dgvDAMenu.Columns[5].Visible = false;
+            //dgvDAMenu.Columns[6].Visible = false;
+            //dgvDAMenu.Columns[7].Visible = false;
+            //dgvDAMenu.Columns[8].Visible = false;
+            //dgvDAMenu.Columns[9].Visible = false;
+            //dgvDAMenu.Columns[10].Visible = false;
+            //dgvDAMenu.Columns[11].Visible = false;
+            //dgvDAMenu.Columns[12].Visible = false;
+            //dgvDAMenu.Columns[13].Visible = false;
+            //dgvDAMenu.Columns[14].Visible = false;
+            //dgvDAMenu.Columns[15].Visible = false;
+            //dgvDAMenu.Columns[16].Visible = false;
+            //dgvDAMenu.Columns[17].Visible = false;
+            //dgvDAMenu.Columns[18].Visible = false;
+            //dgvDAMenu.Columns[19].Visible = false;
+            //dgvDAMenu.Columns[20].Visible = false;
+            //dgvDAMenu.Columns[21].Visible = false;
+            //dgvDAMenu.Columns[22].Visible = false;
+            //dgvDAMenu.Columns[23].Visible = false;
+            //dgvDAMenu.Columns[24].Visible = false;
+            //dgvDAMenu.Columns[25].Visible = false;
             dgvDAMenu.Columns[0].HeaderText = @"Code";
             dgvDAMenu.Columns[0].Width = 40;
             dgvDAMenu.Columns[1].HeaderText = @"ItemName";
@@ -459,6 +482,8 @@ namespace SuperPOS.UI.TakeAway
 
         private void btnDAAdd_Click(object sender, EventArgs e)
         {
+            iStatus = 1;
+
             txtDishCode.Text = "";
             txtDisPosition.Text = "";
             txtEngName.Text = "";
@@ -481,20 +506,214 @@ namespace SuperPOS.UI.TakeAway
             txtSRegular.Enabled = true;
             txtSSmall.Enabled = true;
 
-            chkUnavailable.Checked = false;
-            chkNegativePrice.Checked = false;
-            chkDisountable.Checked = false;
-            chkWithoutVAT.Checked = false;
-            chkInventoryCheck.Checked = false;
             chkUnavailable.Enabled = true;
             chkNegativePrice.Enabled = true;
             chkDisountable.Enabled = true;
             chkWithoutVAT.Enabled = true;
             chkInventoryCheck.Enabled = true;
+            chkUnavailable.Checked = false;
+            chkNegativePrice.Checked = false;
+            chkDisountable.Checked = false;
+            chkWithoutVAT.Checked = false;
+            chkInventoryCheck.Checked = false;
+
+            cmbBoxSplySft.Enabled = true;
+            chkBoxPrtOrderByDept.Enabled = true;
+            chkBoxPrtOrderX.Enabled = true;
+
+            cmbBoxMenuCate1.Enabled = true;
+            cmbBoxMenuCate2.Enabled = true;
+            cmbBoxMenuCate3.Enabled = true;
 
             btnDAEdit.Enabled = false;
             btnDADel.Enabled = false;
+            btnDACopy.Enabled = false;
             btnDASave.Enabled = true;
+        }
+
+        private void BindCmbData()
+        {
+            var lstDeptCate = CommonData.TaMenuCategoryList.OrderBy(s => s.DisplayPosition).Select(lstDC => new { DeptCode = lstDC.EnglishName, SysKey = lstDC.SystemKey });
+            //cmbBoxDeptCode.DataSource = lstDeptCode.ToList();
+            //lstDeptCode.ToList().Add(new { DeptCode = "", SysKey = Guid.Empty });
+
+            cmbBoxMenuCate1.DataSource = lstDeptCate.ToList();
+            cmbBoxMenuCate1.ValueMember = "SysKey";
+            cmbBoxMenuCate1.DisplayMember = "DeptCode";
+            
+
+            cmbBoxMenuCate2.DataSource = lstDeptCate.ToList();
+            cmbBoxMenuCate2.ValueMember = "SysKey";
+            cmbBoxMenuCate2.DisplayMember = "DeptCode";
+
+            cmbBoxMenuCate3.DataSource = lstDeptCate.ToList();
+            cmbBoxMenuCate3.ValueMember = "SysKey";
+            cmbBoxMenuCate3.DisplayMember = "DeptCode";
+
+            var lstSupplySft = CommonData.ShiftCodeList.OrderBy(s => s.DtFrom)
+                    .Select(lstSS => new {sDate = lstSS.ShiftName, sSysKey = lstSS.SystemKey});
+            cmbBoxSplySft.DataSource = lstSupplySft.ToList();
+            cmbBoxSplySft.ValueMember = "sSysKey";
+            cmbBoxSplySft.DisplayMember = "sDate";
+        }
+
+        private void cmbBoxMenuCate1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            //Console.Out.WriteLine(cmbBoxMenuCate1.Text);
+            //Console.Out.WriteLine(cmbBoxMenuCate1.SelectedValue);
+            menuCate1 = cmbBoxMenuCate1.SelectedValue.ToString();
+        }
+
+        private void cmbBoxMenuCate2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            menuCate2 = cmbBoxMenuCate2.SelectedValue.ToString();
+        }
+
+        private void cmbBoxMenuCate3_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            menuCate3 = cmbBoxMenuCate3.SelectedValue.ToString();
+        }
+
+        private void cmbBoxSplySft_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            menuShitCode = cmbBoxSplySft.SelectedValue.ToString();
+        }
+
+        private void btnDAEdit_Click(object sender, EventArgs e)
+        {
+            iStatus = 2;
+
+            btnDAAdd.Enabled = false;
+            btnDACopy.Enabled = false;
+            btnDADel.Enabled = false;
+            btnDASave.Enabled = true;
+
+            txtDishCode.Enabled = true;
+            txtDisPosition.Enabled = true;
+            txtEngName.Enabled = true;
+            txtOtherName.Enabled = true;
+            txtWLarge.Enabled = true;
+            txtWRegular.Enabled = true;
+            txtWSmall.Enabled = true;
+            txtSLarge.Enabled = true;
+            txtSRegular.Enabled = true;
+            txtSSmall.Enabled = true;
+
+            chkUnavailable.Enabled = true;
+            chkNegativePrice.Enabled = true;
+            chkDisountable.Enabled = true;
+            chkWithoutVAT.Enabled = true;
+            chkInventoryCheck.Enabled = true;
+            chkUnavailable.Checked = false;
+            chkNegativePrice.Checked = false;
+            chkDisountable.Checked = false;
+            chkWithoutVAT.Checked = false;
+            chkInventoryCheck.Checked = false;
+
+            cmbBoxMenuCate1.Enabled = true;
+            cmbBoxMenuCate2.Enabled = true;
+            cmbBoxMenuCate3.Enabled = true;
+
+            cmbBoxSplySft.Enabled = true;
+            chkBoxPrtOrderByDept.Enabled = true;
+            chkBoxPrtOrderX.Enabled = true;
+        }
+
+        private void dgvDAMenu_SelectionChanged(object sender, EventArgs e)
+        {
+            if (dgvDAMenu.RowCount == 0)
+            {
+                MessageBox.Show("This table is empty,please add data first!");
+                return;
+            }
+
+            if (dgvDAMenu.CurrentRow == null) return;
+
+            if (dgvDAMenu.CurrentRow.Index < 0) return;
+
+            txtDishCode.Text = dgvDAMenu.CurrentRow.Cells[1].Value.ToString();
+            txtDisPosition.Text = dgvDAMenu.CurrentRow.Cells[2].Value.ToString();
+            txtEngName.Text = dgvDAMenu.CurrentRow.Cells[3].Value.ToString();
+            txtOtherName.Text = dgvDAMenu.CurrentRow.Cells[4].Value.ToString();
+            txtWLarge.Text = dgvDAMenu.CurrentRow.Cells[5].Value.ToString();
+            txtWRegular.Text = dgvDAMenu.CurrentRow.Cells[6].Value.ToString();
+            txtWSmall.Text = dgvDAMenu.CurrentRow.Cells[7].Value.ToString();
+            txtSLarge.Text = dgvDAMenu.CurrentRow.Cells[8].Value.ToString();
+            txtSRegular.Text = dgvDAMenu.CurrentRow.Cells[9].Value.ToString();
+            txtSSmall.Text = dgvDAMenu.CurrentRow.Cells[10].Value.ToString();
+
+            string[] strMenuCate = dgvDAMenu.CurrentRow.Cells[22].Value.ToString().Split(',');
+            cmbBoxMenuCate1.Text = strMenuCate[0];
+            cmbBoxMenuCate2.Text = strMenuCate[1];
+            cmbBoxMenuCate3.Text = strMenuCate[2];
+
+            cmbBoxSplySft.Text = dgvDAMenu.CurrentRow.Cells[12].Value.ToString();
+            chkBoxPrtOrderByDept.Checked = dgvDAMenu.CurrentRow.Cells[13].Value.ToString().Equals("Y");
+            chkBoxPrtOrderX.Checked = dgvDAMenu.CurrentRow.Cells[14].Value.ToString().Equals("Y");
+
+            chkUnavailable.Checked = dgvDAMenu.CurrentRow.Cells[16].Value.ToString().Equals("Y");
+            chkUnavailable.Checked = dgvDAMenu.CurrentRow.Cells[17].Value.ToString().Equals("Y");
+            chkUnavailable.Checked = dgvDAMenu.CurrentRow.Cells[18].Value.ToString().Equals("Y");
+            chkUnavailable.Checked = dgvDAMenu.CurrentRow.Cells[19].Value.ToString().Equals("Y");
+            chkUnavailable.Checked = dgvDAMenu.CurrentRow.Cells[20].Value.ToString().Equals("Y");
+        }
+
+        private void btnDASave_Click(object sender, EventArgs e)
+        {
+            TAMenuItemInfo taMenuItem = new TAMenuItemInfo();
+
+            taMenuItem.DishCode = txtDishCode.Text;
+            taMenuItem.DisplayPosition = txtDisPosition.Text;
+            taMenuItem.EnglishName = txtEngName.Text;
+            taMenuItem.OtherName = txtOtherName.Text;
+            taMenuItem.wLarge = txtWLarge.Text;
+            taMenuItem.wRegular = txtWRegular.Text;
+            taMenuItem.wSmall = txtWSmall.Text;
+            taMenuItem.SpecialLarge = txtSLarge.Text;
+            taMenuItem.SpecialRegular = txtSRegular.Text;
+            taMenuItem.SpecialSmall = txtSSmall.Text;
+
+            taMenuItem.BtnColor = "";
+
+            taMenuItem.IsUnavailable = chkUnavailable.Checked ? "Y" : "N";
+            taMenuItem.IsNegativePrice = chkNegativePrice.Checked ? "Y" : "N";
+            taMenuItem.IsDiscountable = chkDisountable.Checked ? "Y" : "N";
+            taMenuItem.IsWithoutVAT = chkDisountable.Checked ? "Y" : "N";
+            taMenuItem.IsInventoryCheck = chkInventoryCheck.Checked ? "Y" : "N";
+
+            string strMenuCate = "";
+            strMenuCate = cmbBoxMenuCate1.Text + "," + cmbBoxMenuCate2.Text + "," + cmbBoxMenuCate3.Text + ",";
+            taMenuItem.MenuCateID = strMenuCate;
+
+            taMenuItem.SupplyShift = cmbBoxSplySft.Text;
+            taMenuItem.PrinterName = chkBoxPrtOrderByDept.Checked ? "Y" : "N";
+            taMenuItem.PrinterOrder = chkBoxPrtOrderX.Checked ? "Y" : "N";
+
+            taMenuItem.Qty = "0";
+
+            taMenuItem.MenuSetID = "";
+
+            if (iStatus == 1)
+            {
+                taMenuItem.SystemKey = Guid.NewGuid();
+                _control.AddEntity(taMenuItem);
+            }
+            else if (iStatus == 2)
+            {
+                if (dgvDAMenu.CurrentRow != null)
+                {
+                    taMenuItem.SystemKey = new Guid(dgvDAMenu.CurrentRow.Cells[0].Value.ToString());
+                    _control.UpdateEntity(taMenuItem);
+                }
+            }
+
+            new OnLoadSystemCommonData().GetTAMenuItemList();
+            dgvDAMenu.DataSource = CommonData.TaMenuItemList;
+
+            btnDAAdd.Enabled = true;
+            btnDACopy.Enabled = true;
+            btnDADel.Enabled = true;
+            btnDASave.Enabled = false;
         }
     }
 }
