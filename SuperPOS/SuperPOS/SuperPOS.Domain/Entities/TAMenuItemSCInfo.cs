b79320dcb04b657bd11d0e44e476c631c56cb6e8
Loker2015 <Loker2015@d7ed1076-26fe-4ada-867f-e07a5824a29f>
2016-04-15 06:37:00 +0000

    增加TAMenuItemSCInfo,TAMenuItemTCInfo,TAMenuItemTHInfo三个表

---
+++ SuperPOS/SuperPOS/SuperPOS.Domain/Entities/TAMenuItemSCInfo.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace SuperPOS.Domain.Entities
+{
+    public class TAMenuItemSCInfo
+    {
+        [XmlElement(ElementName = "SystemKey")]
+        public virtual Guid SystemKey { get; set; }
+
+        [XmlElement(ElementName = "MenuItemID")]
+        public virtual string MenuItemID { get; set; }
+
+        [XmlElement(ElementName = "MenuSetID")]
+        public virtual string MenuSetID { get; set; }
+
+        [XmlElement(ElementName = "EnglishName")]
+        public virtual string EnglishName { get; set; }
+
+        [XmlElement(ElementName = "OtherName")]
+        public virtual string OtherName { get; set; }
+
+        [XmlElement(ElementName = "AddPrice")]
+        public virtual string AddPrice { get; set; }
+
+        [XmlElement(ElementName = "AutoAppend")]
+        public virtual string AutoAppend { get; set; }
+
+        [XmlElement(ElementName = "IsEnableChoices")]
+        public virtual string IsEnableChoices { get; set; }
+    }
+}
