22eb4fdef1f5601a17d14c9aac894578cfe1329b
superpos <superpos@d7ed1076-26fe-4ada-867f-e07a5824a29f>
2016-04-30 10:58:48 +0000

    修改错别字

--- trunk/SuperPOS/SuperPOS/SuperPOS.UI/TakeAway/FrmTACustInfo.Designer.cs
+++ trunk/SuperPOS/SuperPOS/SuperPOS.UI/TakeAway/FrmTACustInfo.Designer.cs
@@ -101,7 +101,7 @@
             this.groupBox1.Size = new System.Drawing.Size(969, 431);
             this.groupBox1.TabIndex = 0;
             this.groupBox1.TabStop = false;
-            this.groupBox1.Text = "Customer Infomation";
+            this.groupBox1.Text = "Customer Information";
             // 
             // btnExit
             // 
