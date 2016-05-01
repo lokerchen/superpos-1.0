a1da63f96d561293ff5cb880b1c4eda3e28a1114
superpos <superpos@d7ed1076-26fe-4ada-867f-e07a5824a29f>
2016-05-01 04:39:29 +0000

    增加Change Order Type

---
+++ trunk/SuperPOS/SuperPOS/SuperPOS.UI/TakeAway/FrmTAChangeOrderType.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace SuperPOS.UI.TakeAway
+{
+    partial class FrmTAChangeOrderType
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnShop = new System.Windows.Forms.Button();
+            this.btnDelivery = new System.Windows.Forms.Button();
+            this.btnCollection = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnCollection);
+            this.groupBox1.Controls.Add(this.btnDelivery);
+            this.groupBox1.Controls.Add(this.btnShop);
+            this.groupBox1.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.groupBox1.Location = new System.Drawing.Point(4, 4);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(356, 92);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Change order type";
+            // 
+            // btnShop
+            // 
+            this.btnShop.BackColor = System.Drawing.Color.Green;
+            this.btnShop.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnShop.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.btnShop.Location = new System.Drawing.Point(7, 23);
+            this.btnShop.Name = "btnShop";
+            this.btnShop.Size = new System.Drawing.Size(110, 60);
+            this.btnShop.TabIndex = 0;
+            this.btnShop.Text = "Shop";
+            this.btnShop.UseVisualStyleBackColor = false;
+            this.btnShop.Click += new System.EventHandler(this.btnShop_Click);
+            // 
+            // btnDelivery
+            // 
+            this.btnDelivery.BackColor = System.Drawing.Color.Lime;
+            this.btnDelivery.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnDelivery.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.btnDelivery.Location = new System.Drawing.Point(122, 22);
+            this.btnDelivery.Name = "btnDelivery";
+            this.btnDelivery.Size = new System.Drawing.Size(110, 60);
+            this.btnDelivery.TabIndex = 1;
+            this.btnDelivery.Text = "Delivery";
+            this.btnDelivery.UseVisualStyleBackColor = false;
+            this.btnDelivery.Click += new System.EventHandler(this.btnDelivery_Click);
+            // 
+            // btnCollection
+            // 
+            this.btnCollection.BackColor = System.Drawing.Color.Lime;
+            this.btnCollection.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCollection.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.btnCollection.Location = new System.Drawing.Point(239, 22);
+            this.btnCollection.Name = "btnCollection";
+            this.btnCollection.Size = new System.Drawing.Size(110, 60);
+            this.btnCollection.TabIndex = 2;
+            this.btnCollection.Text = "Collection";
+            this.btnCollection.UseVisualStyleBackColor = false;
+            this.btnCollection.Click += new System.EventHandler(this.btnCollection_Click);
+            // 
+            // FrmTAChangeOrderType
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(366, 104);
+            this.Controls.Add(this.groupBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "FrmTAChangeOrderType";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "FrmTAChangeOrderType";
+            this.groupBox1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnCollection;
+        private System.Windows.Forms.Button btnDelivery;
+        private System.Windows.Forms.Button btnShop;
+    }
+}
