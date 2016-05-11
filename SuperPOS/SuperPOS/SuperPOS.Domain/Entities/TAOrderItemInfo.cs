f25fd2c03993271ebe6b0e067d0b0880bb3dba2f
Loker2015 <Loker2015@d7ed1076-26fe-4ada-867f-e07a5824a29f>
2016-05-11 07:56:57 +0000

    增加OrderItem和System Value表

---
+++ SuperPOS/SuperPOS/SuperPOS.Domain/Entities/TAOrderItemInfo.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace SuperPOS.Domain.Entities
+{
+    public class TAOrderItemInfo
+    {
+        [XmlElement(ElementName = "SystemKey")]
+        public virtual Guid SystemKey { get; set; }
+
+        [XmlElement(ElementName = "CheckKey")]
+        public virtual string CheckKey { get; set; }
+
+        [XmlElement(ElementName = "CheckCode")]
+        public virtual string CheckCode { get; set; }
+
+        [XmlElement(ElementName = "ItemCode")]
+        public virtual string ItemCode { get; set; }
+
+        //ItemDishName
+        [XmlElement(ElementName = "ItemDishName")]
+        public virtual string ItemDishName { get; set; }
+
+        [XmlElement(ElementName = "ItemPrice")]
+        public virtual string ItemPrice { get; set; }
+
+        [XmlElement(ElementName = "ItemQty")]
+        public virtual string ItemQty { get; set; }
+
+        [XmlElement(ElementName = "ItemTotalPrice")]
+        public virtual string ItemTotalPrice { get; set; }
+
+        //Remark
+        [XmlElement(ElementName = "Remark")]
+        public virtual string Remark { get; set; }
+    }
+}
