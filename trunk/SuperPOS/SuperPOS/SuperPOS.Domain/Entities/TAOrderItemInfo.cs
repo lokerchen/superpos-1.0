c02459afc12f94ac12db5181e9946142e3b2fa5f
Loker2015 <Loker2015@d7ed1076-26fe-4ada-867f-e07a5824a29f>
2016-05-12 07:52:46 +0000

    调整字段顺序，增加下单时间OrderTime字段

--- trunk/SuperPOS/SuperPOS/SuperPOS.Domain/Entities/TAOrderItemInfo.cs
+++ trunk/SuperPOS/SuperPOS/SuperPOS.Domain/Entities/TAOrderItemInfo.cs
@@ -11,27 +11,30 @@ namespace SuperPOS.Domain.Entities
         [XmlElement(ElementName = "SystemKey")]
         public virtual Guid SystemKey { get; set; }
 
+        [XmlElement(ElementName = "ItemQty")]
+        public virtual string ItemQty { get; set; }
+
+        //ItemDishName
+        [XmlElement(ElementName = "ItemDishName")]
+        public virtual string ItemDishName { get; set; }
+
+        [XmlElement(ElementName = "ItemTotalPrice")]
+        public virtual string ItemTotalPrice { get; set; }
+
+        [XmlElement(ElementName = "ItemCode")]
+        public virtual string ItemCode { get; set; }
+
+        [XmlElement(ElementName = "ItemPrice")]
+        public virtual string ItemPrice { get; set; }
+
         [XmlElement(ElementName = "CheckKey")]
         public virtual string CheckKey { get; set; }
 
         [XmlElement(ElementName = "CheckCode")]
         public virtual string CheckCode { get; set; }
 
-        [XmlElement(ElementName = "ItemCode")]
-        public virtual string ItemCode { get; set; }
-
-        //ItemDishName
-        [XmlElement(ElementName = "ItemDishName")]
-        public virtual string ItemDishName { get; set; }
-
-        [XmlElement(ElementName = "ItemPrice")]
-        public virtual string ItemPrice { get; set; }
-
-        [XmlElement(ElementName = "ItemQty")]
-        public virtual string ItemQty { get; set; }
-
-        [XmlElement(ElementName = "ItemTotalPrice")]
-        public virtual string ItemTotalPrice { get; set; }
+        [XmlElement(ElementName = "OrderTime")]
+        public virtual string OrderTime { get; set; }
 
         //Remark
         [XmlElement(ElementName = "Remark")]
