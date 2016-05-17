533ccf14dc6418dbc9902ee6a511fd40660e4052
Loker2015 <Loker2015@d7ed1076-26fe-4ada-867f-e07a5824a29f>
2016-05-17 08:57:32 +0000

    增加Customer Number:客户编号传入

--- SuperPOS/SuperPOS/SuperPOS.UI/TakeAway/FrmTAPay.cs
+++ SuperPOS/SuperPOS/SuperPOS.UI/TakeAway/FrmTAPay.cs
@@ -6,6 +6,8 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using SuperPOS.DAL;
+using SuperPOS.Domain.Entities;
 
 namespace SuperPOS.UI.TakeAway
 {
@@ -13,15 +15,18 @@ namespace SuperPOS.UI.TakeAway
     {
         //订单编号
         private string chkNum = "";
+        //会员编号
+        private string CustNum = "";
         public FrmTAPay()
         {
             InitializeComponent();
         }
 
-        public FrmTAPay(string strChkNum)
+        public FrmTAPay(string strChkNum, string strCustNum)
         {
             InitializeComponent();
             chkNum = strChkNum;
+            CustNum = strCustNum;
         }
 
         #region 退出
@@ -33,7 +38,27 @@ namespace SuperPOS.UI.TakeAway
 
         private void FrmTAPay_Load(object sender, EventArgs e)
         {
+            //查询会员
+            new OnLoadSystemCommonData().GetTACust();
 
+            var custList = CommonData.TaCustList.Where(s => s.SystemKey.ToString().Equals(CustNum));
+
+            if (custList.Any())
+            {
+                TACustInfo taCustInfo = custList.FirstOrDefault();
+                txtPhone.Text = taCustInfo.Phone1;
+                txtName.Text = taCustInfo.Name;
+                txtHouseNo.Text = taCustInfo.HouseNo;
+                txtAddress.Text = taCustInfo.Address1;
+                txtPostcode.Text = taCustInfo.Postcode1;
+                txtDistance.Text = taCustInfo.Distance;
+                txtPCZone.Text = taCustInfo.PcZone;
+                txtDelCharge.Text = taCustInfo.DelCharge;
+                txtReadyTime.Text = taCustInfo.ReadyTime;
+                txtIntNotes.Text = taCustInfo.IntNotes;
+                txtNotes.Text = taCustInfo.NotesOnBill;
+                chkBlackListed.Checked = taCustInfo.IsBlackListed.Equals("Y");
+            }
         }
     }
 }
