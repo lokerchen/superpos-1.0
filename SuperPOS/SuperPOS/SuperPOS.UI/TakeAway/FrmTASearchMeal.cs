7b39596b21a06fa82f00694c1884bf8ab459c738
Loker2015 <Loker2015@d7ed1076-26fe-4ada-867f-e07a5824a29f>
2016-05-31 06:45:57 +0000

    增加查询菜品关键字窗口

---
+++ SuperPOS/SuperPOS/SuperPOS.UI/TakeAway/FrmTASearchMeal.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SuperPOS.UI.TakeAway
+{
+    public partial class FrmTASearchMeal : Form
+    {
+        public string ValueString
+        {
+            get { return txtKeyWord.Text; }
+            set { ValueString = value; }
+        }
+
+        public FrmTASearchMeal()
+        {
+            InitializeComponent();
+        }
+
+        private void btnOK_Click(object sender, EventArgs e)
+        {
+            this.DialogResult = DialogResult.OK;
+            Hide();
+        }
+    }
+}
