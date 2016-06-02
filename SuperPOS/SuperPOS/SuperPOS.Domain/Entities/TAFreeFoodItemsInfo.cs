33fcb671899fda34fca19c1e3470b07ec3960fa4
Loker2015 <Loker2015@d7ed1076-26fe-4ada-867f-e07a5824a29f>
2016-06-02 07:31:03 +0000

    增加Free Food Item表

---
+++ SuperPOS/SuperPOS/SuperPOS.Domain/Entities/TAFreeFoodItemsInfo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace SuperPOS.Domain.Entities
+{
+    public class TAFreeFoodItemsInfo
+    {
+        [XmlElement(ElementName = "SystemKey")]
+        public virtual Guid SystemKey { get; set; }
+
+        [XmlElement(ElementName = "FreeAmount")]
+        public virtual string FreeAmount { get; set; }
+
+        [XmlElement(ElementName = "FreeCode")]
+        public virtual string FreeCode { get; set; }
+
+        [XmlElement(ElementName = "Remark")]
+        public virtual string Remark { get; set; }
+    }
+}
