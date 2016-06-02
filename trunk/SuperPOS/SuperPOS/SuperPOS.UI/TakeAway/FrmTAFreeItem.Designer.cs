1f3f1acba39848d01fe7e9ea22423135bae544b8
Loker2015 <Loker2015@d7ed1076-26fe-4ada-867f-e07a5824a29f>
2016-06-02 08:45:43 +0000

    增加前台FreeItem窗口

---
+++ trunk/SuperPOS/SuperPOS/SuperPOS.UI/TakeAway/FrmTAFreeItem.Designer.cs
@@ -0,0 +1,165 @@
+﻿namespace SuperPOS.UI.TakeAway
+{
+    partial class FrmTAFreeItem
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnFreeItem4 = new System.Windows.Forms.Button();
+            this.btnFreeItem3 = new System.Windows.Forms.Button();
+            this.btnFreeItem2 = new System.Windows.Forms.Button();
+            this.btnFreeItem1 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnCancel);
+            this.groupBox1.Controls.Add(this.btnSave);
+            this.groupBox1.Controls.Add(this.btnFreeItem4);
+            this.groupBox1.Controls.Add(this.btnFreeItem3);
+            this.groupBox1.Controls.Add(this.btnFreeItem2);
+            this.groupBox1.Controls.Add(this.btnFreeItem1);
+            this.groupBox1.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.groupBox1.Location = new System.Drawing.Point(5, 3);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(300, 179);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Free Items";
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.btnCancel.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnCancel.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.btnCancel.Location = new System.Drawing.Point(160, 125);
+            this.btnCancel.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(129, 40);
+            this.btnCancel.TabIndex = 20;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = false;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.btnSave.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnSave.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.btnSave.Location = new System.Drawing.Point(10, 125);
+            this.btnSave.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(129, 40);
+            this.btnSave.TabIndex = 19;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = false;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // btnFreeItem4
+            // 
+            this.btnFreeItem4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.btnFreeItem4.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnFreeItem4.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.btnFreeItem4.Location = new System.Drawing.Point(160, 77);
+            this.btnFreeItem4.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.btnFreeItem4.Name = "btnFreeItem4";
+            this.btnFreeItem4.Size = new System.Drawing.Size(129, 40);
+            this.btnFreeItem4.TabIndex = 18;
+            this.btnFreeItem4.Text = "Mixed hot platter";
+            this.btnFreeItem4.UseVisualStyleBackColor = false;
+            // 
+            // btnFreeItem3
+            // 
+            this.btnFreeItem3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.btnFreeItem3.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnFreeItem3.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.btnFreeItem3.Location = new System.Drawing.Point(10, 77);
+            this.btnFreeItem3.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.btnFreeItem3.Name = "btnFreeItem3";
+            this.btnFreeItem3.Size = new System.Drawing.Size(129, 40);
+            this.btnFreeItem3.TabIndex = 17;
+            this.btnFreeItem3.Text = "Mixed hot platter";
+            this.btnFreeItem3.UseVisualStyleBackColor = false;
+            // 
+            // btnFreeItem2
+            // 
+            this.btnFreeItem2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.btnFreeItem2.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnFreeItem2.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.btnFreeItem2.Location = new System.Drawing.Point(160, 29);
+            this.btnFreeItem2.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.btnFreeItem2.Name = "btnFreeItem2";
+            this.btnFreeItem2.Size = new System.Drawing.Size(129, 40);
+            this.btnFreeItem2.TabIndex = 16;
+            this.btnFreeItem2.Text = "Mixed hot platter";
+            this.btnFreeItem2.UseVisualStyleBackColor = false;
+            // 
+            // btnFreeItem1
+            // 
+            this.btnFreeItem1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.btnFreeItem1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnFreeItem1.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.btnFreeItem1.Location = new System.Drawing.Point(10, 29);
+            this.btnFreeItem1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.btnFreeItem1.Name = "btnFreeItem1";
+            this.btnFreeItem1.Size = new System.Drawing.Size(129, 40);
+            this.btnFreeItem1.TabIndex = 15;
+            this.btnFreeItem1.Text = "Mixed hot platter";
+            this.btnFreeItem1.UseVisualStyleBackColor = false;
+            // 
+            // FrmTAFreeItem
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(312, 192);
+            this.Controls.Add(this.groupBox1);
+            this.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "FrmTAFreeItem";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "FrmTAFreeItem";
+            this.Load += new System.EventHandler(this.FrmTAFreeItem_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnFreeItem4;
+        private System.Windows.Forms.Button btnFreeItem3;
+        private System.Windows.Forms.Button btnFreeItem2;
+        private System.Windows.Forms.Button btnFreeItem1;
+    }
+}
---
+++ trunk/SuperPOS/SuperPOS/SuperPOS.UI/TakeAway/FrmTAFreeItem.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using SuperPOS.DAL;
+using SuperPOS.Domain.Entities;
+
+namespace SuperPOS.UI.TakeAway
+{
+    public partial class FrmTAFreeItem : Form
+    {
+        private Button[] btnFreeItem = new Button[4];
+        private string[] strDishCode = new string[4];
+        private string sDishCode = "";
+
+        public FrmTAFreeItem()
+        {
+            InitializeComponent();
+        }
+
+        public string DishCode
+        {
+            get { return sDishCode; }
+            set { DishCode = value; }
+        }
+
+        private void FrmTAFreeItem_Load(object sender, EventArgs e)
+        {
+            btnFreeItem[0] = btnFreeItem1;
+            btnFreeItem[1] = btnFreeItem2;
+            btnFreeItem[2] = btnFreeItem3;
+            btnFreeItem[3] = btnFreeItem4;
+
+            btnFreeItem1.Click += BtnFreeItem_Click;
+            btnFreeItem2.Click += BtnFreeItem_Click;
+            btnFreeItem3.Click += BtnFreeItem_Click;
+            btnFreeItem4.Click += BtnFreeItem_Click;
+
+            new OnLoadSystemCommonData().GetTAFreeFoodItemsList();
+            new OnLoadSystemCommonData().GetTAMenuItemList();
+
+            int i = 0;
+            foreach (var taFreeFoodItemsInfo in CommonData.TAFreeFoodItems)
+            {
+                if (i >= 4) break;
+
+                if (!string.IsNullOrEmpty(taFreeFoodItemsInfo.FreeCode))
+                {
+                    if (CommonData.TaMenuItemList.Any(s => s.DishCode.Equals(taFreeFoodItemsInfo.FreeCode)))
+                    {
+                        TAMenuItemInfo taMenuItemInfo = CommonData.TaMenuItemList.FirstOrDefault(s => s.DishCode.Equals(taFreeFoodItemsInfo.FreeCode));
+                        btnFreeItem[i].Text = taMenuItemInfo.EnglishName;
+                        strDishCode[i] = taFreeFoodItemsInfo.FreeCode;
+                    }
+                }
+
+                i++;
+            }
+
+            for (int j = i; j < 4; j++)
+            {
+                btnFreeItem[i].Visible = false;
+                strDishCode[i] = "";
+            }
+        }
+
+        private void BtnFreeItem_Click(object sender, EventArgs e)
+        {
+            Button btn = sender as Button;
+
+            if (btn.Name.Equals("btnFreeItem1"))
+            {
+                btnFreeItem1.BackColor = Color.FromArgb(128, 128, 255);
+                btnFreeItem2.BackColor = Color.FromArgb(225, 225, 128);
+                btnFreeItem3.BackColor = Color.FromArgb(225, 225, 128);
+                btnFreeItem4.BackColor = Color.FromArgb(225, 225, 128);
+                sDishCode = strDishCode[0];
+            }
+            else if (btn.Name.Equals("btnFreeItem2"))
+            {
+                btnFreeItem1.BackColor = Color.FromArgb(225, 225, 128);
+                btnFreeItem2.BackColor = Color.FromArgb(128, 128, 255); 
+                btnFreeItem3.BackColor = Color.FromArgb(225, 225, 128);
+                btnFreeItem4.BackColor = Color.FromArgb(225, 225, 128);
+                sDishCode = strDishCode[1];
+            }
+            else if (btn.Name.Equals("btnFreeItem3"))
+            {
+                btnFreeItem1.BackColor = Color.FromArgb(225, 225, 128);
+                btnFreeItem2.BackColor = Color.FromArgb(225, 225, 128);
+                btnFreeItem3.BackColor = Color.FromArgb(128, 128, 255); 
+                btnFreeItem4.BackColor = Color.FromArgb(225, 225, 128);
+                sDishCode = strDishCode[2];
+            }
+            else if (btn.Name.Equals("btnFreeItem4"))
+            {
+                btnFreeItem1.BackColor = Color.FromArgb(225, 225, 128);
+                btnFreeItem2.BackColor = Color.FromArgb(225, 225, 128);
+                btnFreeItem3.BackColor = Color.FromArgb(225, 225, 128);
+                btnFreeItem4.BackColor = Color.FromArgb(128, 128, 255);
+                sDishCode = strDishCode[3];
+            }
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            this.DialogResult = DialogResult.OK;
+            this.Hide();
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            sDishCode = "";
+            this.Hide();
+        }
+    }
+}
