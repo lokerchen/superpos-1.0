dfcffd6a5c4f5b747421349770798f1ae95f3266
Loker2015 <Loker2015@d7ed1076-26fe-4ada-867f-e07a5824a29f>
2016-06-16 09:03:51 +0000

    增加打印中心处理类

---
+++ SuperPOS/SuperPOS/SuperPOS.UI/Print/PrtStatic.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SuperPOS.UI.Print
+{
+    public class PrtStatic
+    {
+        //每行打印字数
+        public int PRT_LINE_SIZE = 39;
+
+        //打印行高 1/100英寸
+        public int PRT_LINE_HEIGHT = 100;
+
+        //字体大小 1/10英寸
+        public int PRT_FONT_SIZE = 10;
+    }
+}
