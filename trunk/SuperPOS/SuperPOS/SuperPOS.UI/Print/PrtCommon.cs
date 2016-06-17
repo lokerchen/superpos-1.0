80dc8ea4b5f3da8e4ef61ce3b5761e82a33b96cc
Loker2015 <Loker2015@d7ed1076-26fe-4ada-867f-e07a5824a29f>
2016-06-17 05:43:30 +0000

    增加部分代码

--- trunk/SuperPOS/SuperPOS/SuperPOS.UI/Print/PrtCommon.cs
+++ trunk/SuperPOS/SuperPOS/SuperPOS.UI/Print/PrtCommon.cs
@@ -3,10 +3,125 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using SuperPOS.DAL;
+using SuperPOS.Domain.Entities;
 
 namespace SuperPOS.UI.Print
 {
     public class PrtCommon
     {
+        #region 打印分隔符
+        /// <summary>
+        /// 打印分隔符
+        /// </summary>
+        /// <returns></returns>
+        private string GetSplit()
+        {
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < PrtStatic.PRT_LINE_SIZE; i++)
+            {
+                sb.Append(PrtStatic.PRT_SPLIT_CHAR);
+            }
+
+            return sb.ToString();
+        }
+        #endregion
+
+        #region 打印空格
+        /// <summary>
+        /// 打印空格
+        /// </summary>
+        /// <param name="i">空格数量</param>
+        /// <returns></returns>
+        public StringBuilder GetSpace(int i)
+        {
+            StringBuilder sb = new StringBuilder();
+            for (int j = 0; j < i; j++)
+            {
+                sb.Append(' ');
+            }
+            return sb;
+        }
+        #endregion
+
+        #region 打印订单抬头
+
+
+        #endregion
+
+        #region 获得餐厅名称
+        /// <summary>
+        /// 获得餐厅名称
+        /// </summary>
+        /// <returns></returns>
+        public string GetRestName()
+        {
+            new OnLoadSystemCommonData().GetSysControlList();
+            var firstOrDefault = CommonData.SysControlList.FirstOrDefault();
+            return firstOrDefault != null ? firstOrDefault.ShopName : "";
+        }
+        #endregion
+
+        #region 获得餐厅地址
+        /// <summary>
+        /// 获得餐厅地址
+        /// </summary>
+        /// <returns></returns>
+        public string GetRestAddr()
+        {
+            new OnLoadSystemCommonData().GetSysControlList();
+            var firstOrDefault = CommonData.SysControlList.FirstOrDefault();
+            return firstOrDefault != null ? firstOrDefault.Address : "";
+        }
+        #endregion
+
+        #region 获得餐厅电话
+        /// <summary>
+        /// 获得餐厅电话
+        /// </summary>
+        /// <returns></returns>
+        public string GetRestTel()
+        {
+            new OnLoadSystemCommonData().GetTAPrtInfo();
+            var firstOrDefault = CommonData.TAPrtInfos.FirstOrDefault();
+            return firstOrDefault != null ? "Tel:" + firstOrDefault.TelNo : "";
+        }
+        #endregion
+
+        #region 获得餐厅VAT NO
+        /// <summary>
+        /// 获得餐厅VAT NO
+        /// </summary>
+        /// <returns></returns>
+        public string GetRestVATNo()
+        {
+            new OnLoadSystemCommonData().GetTAPrtInfo();
+            var firstOrDefault = CommonData.TAPrtInfos.FirstOrDefault();
+            return firstOrDefault != null ? "VAT No:" + firstOrDefault.VATNo : "";
+        }
+        #endregion
+
+        #region 获得打印时间和日期
+        /// <summary>
+        /// 打印时间和日期
+        /// </summary>
+        /// <returns></returns>
+        public string GetPrtDateTime()
+        {
+            return "Date:" + DateTime.Now.ToShortDateString() + "  Time:" + DateTime.Now.ToShortTimeString();
+        }
+        #endregion
+        
+        #region 打印Bill-双语
+
+        public string GetPrintBillBilingual()
+        {
+            StringBuilder sb = new StringBuilder();
+            string L = GetSplit();
+            string L1 = "";
+
+            return sb.ToString();
+        }
+        #endregion
     }
 }
--- trunk/SuperPOS/SuperPOS/SuperPOS.UI/Print/PrtStatic.cs
+++ trunk/SuperPOS/SuperPOS/SuperPOS.UI/Print/PrtStatic.cs
@@ -8,13 +8,22 @@ namespace SuperPOS.UI.Print
 {
     public class PrtStatic
     {
-        //每行打印字数
-        public int PRT_LINE_SIZE = 39;
+        //10号字体每行最多可打印字符数
+        public static int PRT_LINE_SIZE = 39;
 
         //打印行高 1/100英寸
-        public int PRT_LINE_HEIGHT = 100;
+        public static int PRT_LINE_HEIGHT = 100;
 
         //字体大小 1/10英寸
-        public int PRT_FONT_SIZE = 10;
+        public static int PRT_FONT_SIZE = 10;
+
+        //分割符
+        public static string PRT_SPLIT_CHAR = "-";
+
+        //15号字体每行最多可打印字符数
+        public static int PRT_LINE_SIZE_15 = 27;
+
+        //20号字体每行最多可打印字符数
+        public static int PRT_LINE_SIZE_20 = 21;
     }
 }
