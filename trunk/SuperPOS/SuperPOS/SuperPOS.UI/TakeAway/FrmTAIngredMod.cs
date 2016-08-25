66b7e1a54e4a1cb5ee1182d339c068cc4d38c82a
superpos <superpos@d7ed1076-26fe-4ada-867f-e07a5824a29f>
2016-08-25 14:04:03 +0000

    1.增加Ingred Mode按钮功能 2.新增Ingred Mode窗口

---
+++ trunk/SuperPOS/SuperPOS/SuperPOS.UI/TakeAway/FrmTAIngredMod.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SuperPOS.UI.TakeAway
+{
+    public partial class FrmTAIngredMod : Form
+    {
+        private string strValue = ""; 
+
+        public FrmTAIngredMod()
+        {
+            InitializeComponent();
+        }
+
+        public string ValueString
+        {
+            get { return strValue; }
+            set { ValueString = value; }
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.DialogResult = DialogResult.OK;
+            Hide();
+        }
+
+        private void btnNo_Click(object sender, EventArgs e)
+        {
+            strValue = "X";
+            this.DialogResult = DialogResult.OK;
+            Hide();
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            strValue = "+";
+            this.DialogResult = DialogResult.OK;
+            Hide();
+        }
+
+        private void btnChange_Click(object sender, EventArgs e)
+        {
+            strValue = "/";
+            this.DialogResult = DialogResult.OK;
+            Hide();
+        }
+    }
+}
