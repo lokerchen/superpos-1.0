3a9712e83a6d04c9fe2c281002cd55f8f89ab615
superpos <superpos@d7ed1076-26fe-4ada-867f-e07a5824a29f>
2017-05-03 15:06:46 +0000

    增加Second Choices和Third Choices窗口

---
+++ SuperPOS/SuperPOS/SuperPOS.UI/TakeAway/Frm3rdChoice.Designer.cs
@@ -0,0 +1,274 @@
+﻿namespace SuperPOS.UI.TakeAway
+{
+    partial class Frm3rdChoice
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnExit = new System.Windows.Forms.Button();
+            this.btn15 = new System.Windows.Forms.Button();
+            this.btn14 = new System.Windows.Forms.Button();
+            this.btn13 = new System.Windows.Forms.Button();
+            this.btn12 = new System.Windows.Forms.Button();
+            this.btn11 = new System.Windows.Forms.Button();
+            this.btn10 = new System.Windows.Forms.Button();
+            this.btn9 = new System.Windows.Forms.Button();
+            this.btn8 = new System.Windows.Forms.Button();
+            this.btn7 = new System.Windows.Forms.Button();
+            this.btn6 = new System.Windows.Forms.Button();
+            this.btn5 = new System.Windows.Forms.Button();
+            this.btn4 = new System.Windows.Forms.Button();
+            this.btn3 = new System.Windows.Forms.Button();
+            this.btn2 = new System.Windows.Forms.Button();
+            this.btn1 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(760, 30);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(104, 185);
+            this.btnExit.TabIndex = 15;
+            this.btnExit.Text = "EXIT";
+            this.btnExit.UseVisualStyleBackColor = true;
+            // 
+            // btn15
+            // 
+            this.btn15.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.btn15.Location = new System.Drawing.Point(610, 165);
+            this.btn15.Name = "btn15";
+            this.btn15.Size = new System.Drawing.Size(130, 50);
+            this.btn15.TabIndex = 14;
+            this.btn15.Text = "button11";
+            this.btn15.UseVisualStyleBackColor = true;
+            // 
+            // btn14
+            // 
+            this.btn14.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.btn14.Location = new System.Drawing.Point(460, 165);
+            this.btn14.Name = "btn14";
+            this.btn14.Size = new System.Drawing.Size(130, 50);
+            this.btn14.TabIndex = 13;
+            this.btn14.Text = "button12";
+            this.btn14.UseVisualStyleBackColor = true;
+            // 
+            // btn13
+            // 
+            this.btn13.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.btn13.Location = new System.Drawing.Point(310, 165);
+            this.btn13.Name = "btn13";
+            this.btn13.Size = new System.Drawing.Size(130, 50);
+            this.btn13.TabIndex = 12;
+            this.btn13.Text = "button13";
+            this.btn13.UseVisualStyleBackColor = true;
+            // 
+            // btn12
+            // 
+            this.btn12.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.btn12.Location = new System.Drawing.Point(160, 165);
+            this.btn12.Name = "btn12";
+            this.btn12.Size = new System.Drawing.Size(130, 50);
+            this.btn12.TabIndex = 11;
+            this.btn12.Text = "button14";
+            this.btn12.UseVisualStyleBackColor = true;
+            // 
+            // btn11
+            // 
+            this.btn11.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.btn11.Location = new System.Drawing.Point(10, 165);
+            this.btn11.Name = "btn11";
+            this.btn11.Size = new System.Drawing.Size(130, 50);
+            this.btn11.TabIndex = 10;
+            this.btn11.Text = "button15";
+            this.btn11.UseVisualStyleBackColor = true;
+            // 
+            // btn10
+            // 
+            this.btn10.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.btn10.Location = new System.Drawing.Point(610, 96);
+            this.btn10.Name = "btn10";
+            this.btn10.Size = new System.Drawing.Size(130, 50);
+            this.btn10.TabIndex = 9;
+            this.btn10.Text = "button6";
+            this.btn10.UseVisualStyleBackColor = true;
+            // 
+            // btn9
+            // 
+            this.btn9.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.btn9.Location = new System.Drawing.Point(460, 96);
+            this.btn9.Name = "btn9";
+            this.btn9.Size = new System.Drawing.Size(130, 50);
+            this.btn9.TabIndex = 8;
+            this.btn9.Text = "button7";
+            this.btn9.UseVisualStyleBackColor = true;
+            // 
+            // btn8
+            // 
+            this.btn8.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.btn8.Location = new System.Drawing.Point(310, 96);
+            this.btn8.Name = "btn8";
+            this.btn8.Size = new System.Drawing.Size(130, 50);
+            this.btn8.TabIndex = 7;
+            this.btn8.Text = "button8";
+            this.btn8.UseVisualStyleBackColor = true;
+            // 
+            // btn7
+            // 
+            this.btn7.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.btn7.Location = new System.Drawing.Point(160, 96);
+            this.btn7.Name = "btn7";
+            this.btn7.Size = new System.Drawing.Size(130, 50);
+            this.btn7.TabIndex = 6;
+            this.btn7.Text = "button9";
+            this.btn7.UseVisualStyleBackColor = true;
+            // 
+            // btn6
+            // 
+            this.btn6.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.btn6.Location = new System.Drawing.Point(10, 96);
+            this.btn6.Name = "btn6";
+            this.btn6.Size = new System.Drawing.Size(130, 50);
+            this.btn6.TabIndex = 5;
+            this.btn6.Text = "button10";
+            this.btn6.UseVisualStyleBackColor = true;
+            // 
+            // btn5
+            // 
+            this.btn5.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.btn5.Location = new System.Drawing.Point(610, 30);
+            this.btn5.Name = "btn5";
+            this.btn5.Size = new System.Drawing.Size(130, 50);
+            this.btn5.TabIndex = 4;
+            this.btn5.Text = "button5";
+            this.btn5.UseVisualStyleBackColor = true;
+            // 
+            // btn4
+            // 
+            this.btn4.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.btn4.Location = new System.Drawing.Point(460, 30);
+            this.btn4.Name = "btn4";
+            this.btn4.Size = new System.Drawing.Size(130, 50);
+            this.btn4.TabIndex = 3;
+            this.btn4.Text = "button3";
+            this.btn4.UseVisualStyleBackColor = true;
+            // 
+            // btn3
+            // 
+            this.btn3.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.btn3.Location = new System.Drawing.Point(310, 30);
+            this.btn3.Name = "btn3";
+            this.btn3.Size = new System.Drawing.Size(130, 50);
+            this.btn3.TabIndex = 2;
+            this.btn3.Text = "button4";
+            this.btn3.UseVisualStyleBackColor = true;
+            // 
+            // btn2
+            // 
+            this.btn2.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.btn2.Location = new System.Drawing.Point(160, 30);
+            this.btn2.Name = "btn2";
+            this.btn2.Size = new System.Drawing.Size(130, 50);
+            this.btn2.TabIndex = 1;
+            this.btn2.Text = "btn2";
+            this.btn2.UseVisualStyleBackColor = true;
+            // 
+            // btn1
+            // 
+            this.btn1.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.btn1.Location = new System.Drawing.Point(10, 30);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(130, 50);
+            this.btn1.TabIndex = 0;
+            this.btn1.Text = "btn1";
+            this.btn1.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnExit);
+            this.groupBox1.Controls.Add(this.btn15);
+            this.groupBox1.Controls.Add(this.btn14);
+            this.groupBox1.Controls.Add(this.btn13);
+            this.groupBox1.Controls.Add(this.btn12);
+            this.groupBox1.Controls.Add(this.btn11);
+            this.groupBox1.Controls.Add(this.btn10);
+            this.groupBox1.Controls.Add(this.btn9);
+            this.groupBox1.Controls.Add(this.btn8);
+            this.groupBox1.Controls.Add(this.btn7);
+            this.groupBox1.Controls.Add(this.btn6);
+            this.groupBox1.Controls.Add(this.btn5);
+            this.groupBox1.Controls.Add(this.btn4);
+            this.groupBox1.Controls.Add(this.btn3);
+            this.groupBox1.Controls.Add(this.btn2);
+            this.groupBox1.Controls.Add(this.btn1);
+            this.groupBox1.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.groupBox1.Location = new System.Drawing.Point(4, 3);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(882, 229);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Third Choices";
+            // 
+            // Frm3rdChoice
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(892, 242);
+            this.Controls.Add(this.groupBox1);
+            this.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "Frm3rdChoice";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Frm3rdChoice";
+            this.Load += new System.EventHandler(this.Frm3rdChoice_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Button btn15;
+        private System.Windows.Forms.Button btn14;
+        private System.Windows.Forms.Button btn13;
+        private System.Windows.Forms.Button btn12;
+        private System.Windows.Forms.Button btn11;
+        private System.Windows.Forms.Button btn10;
+        private System.Windows.Forms.Button btn9;
+        private System.Windows.Forms.Button btn8;
+        private System.Windows.Forms.Button btn7;
+        private System.Windows.Forms.Button btn6;
+        private System.Windows.Forms.Button btn5;
+        private System.Windows.Forms.Button btn4;
+        private System.Windows.Forms.Button btn3;
+        private System.Windows.Forms.Button btn2;
+        private System.Windows.Forms.Button btn1;
+        private System.Windows.Forms.GroupBox groupBox1;
+    }
+}
